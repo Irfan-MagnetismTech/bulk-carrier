af503ce47a828519ebf93b2bf245e6ecb054f8c0
rabiuli <robiuli913@gmail.com>
2023-08-27 15:34:47 +0600

    OPS: vessels api added

--- backend/Modules/Operations/Http/Controllers/OpsVesselController.php
+++ backend/Modules/Operations/Http/Controllers/OpsVesselController.php
@@ -27,22 +27,22 @@ class OpsVesselController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    // public function index(): JsonResponse
-    // {
-    //     try
-    //     {
-    //         $vessel = OpsVessel::orderBy('id','DESC')->Paginate();
+    public function index(): JsonResponse
+    {
+        try
+        {
+            $vessel = OpsVessel::orderBy('id','DESC')->Paginate();
 
-    //         return response()->json([
-    //             'value'   => $vessel,
-    //             'message' => 'Successfully retrieved vessels.',
-    //         ], 200);
-    //     }
-    //     catch (\Exception $e)
-    //     {
-    //         return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
-    //     }
-    // }
+            return response()->json([
+                'value'   => $vessel,
+                'message' => 'Successfully retrieved vessels.',
+            ], 200);
+        }
+        catch (\Exception $e)
+        {
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
+    }
 
     /**
      * Store a newly created vessel in storage.
@@ -50,23 +50,23 @@ class OpsVesselController extends Controller
      * @param  OpsVesselRequest  $request
      * @return JsonResponse
      */
-    // public function store(OpsVesselRequest $request): JsonResponse
-    // {
-    //     dd($request);
-    //     try
-    //     {
-    //         $vessel = OpsVessel::create($request->all());
+    public function store(OpsVesselRequest $request): JsonResponse
+    {
+        dd($request);
+        try
+        {
+            $vessel = OpsVessel::create($request->all());
 
-    //         return response()->json([
-    //             'value'   => $vessel,
-    //             'message' => 'Successfully created vessel.',
-    //         ], 201);
-    //     }
-    //     catch (\Exception $e)
-    //     {
-    //         return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
-    //     }
-    // }
+            return response()->json([
+                'value'   => $vessel,
+                'message' => 'Successfully created vessel.',
+            ], 201);
+        }
+        catch (\Exception $e)
+        {
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
+    }
 
     /**
      * Display the specified vessel.
@@ -74,21 +74,21 @@ class OpsVesselController extends Controller
      * @param  OpsVessel  $vessel
      * @return JsonResponse
      */
-    // public function show(OpsVessel $vessel): JsonResponse
-    // {
-    //     try
-    //     {
-    //         return response()->json([
-    //             'value'   => $vessel,
-    //             'message' => 'Successfully retrieved vessel.',
-    //         ], 200);
-    //     }
-    //     catch (\Exception$e)
-    //     {
-    //         return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
-    //     }
+    public function show(OpsVessel $vessel): JsonResponse
+    {
+        try
+        {
+            return response()->json([
+                'value'   => $vessel,
+                'message' => 'Successfully retrieved vessel.',
+            ], 200);
+        }
+        catch (\Exception$e)
+        {
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
 
-    // }
+    }
 
     /**
      * Update the specified vessel in storage.
@@ -97,21 +97,21 @@ class OpsVesselController extends Controller
      * @param  OpsVessel  $vessel
      * @return JsonResponse
      */
-    // public function update(OpsVesselRequest $request, OpsVessel $vessel): JsonResponse
-    // {
-    //     try
-    //     {
-    //         $vessel->update($request->all());
+    public function update(OpsVesselRequest $request, OpsVessel $vessel): JsonResponse
+    {
+        try
+        {
+            $vessel->update($request->all());
 
-    //         return response()->json([
-    //             'message' => 'Successfully updated vessel.',
-    //         ], 202);
-    //     }
-    //     catch (\Exception$e)
-    //     {
-    //         return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
-    //     }
-    // }
+            return response()->json([
+                'message' => 'Successfully updated vessel.',
+            ], 202);
+        }
+        catch (\Exception$e)
+        {
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
+    }
 
     /**
      * Remove the specified vessel from storage.
@@ -119,62 +119,62 @@ class OpsVesselController extends Controller
      * @param  OpsVessel  $vessel
      * @return \Illuminate\Http\JsonResponse
      */
-    // public function destroy(OpsVessel $vessel): JsonResponse
-    // {
-    //     try
-    //     {
-    //         $vessel->delete();
+    public function destroy(OpsVessel $vessel): JsonResponse
+    {
+        try
+        {
+            $vessel->delete();
 
-    //         return response()->json([
-    //             'message' => 'Successfully deleted vessel.',
-    //         ], 204);
-    //     }
-    //     catch (\Exception$e)
-    //     {
-    //         return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
-    //     }
-    // }
+            return response()->json([
+                'message' => 'Successfully deleted vessel.',
+            ], 204);
+        }
+        catch (\Exception$e)
+        {
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
+    }
     
-    // public function search(Request $request) {
-    //     try {
-    //         $vessel = OpsVessel::where('name', 'like', '%' . $request->search . '%')->get();
+    public function search(Request $request) {
+        try {
+            $vessel = OpsVessel::where('name', 'like', '%' . $request->search . '%')->get();
 
-    //         return response()->json([
-    //             'value'   => $vessel,
-    //             'message' => 'Successfully retrieved vessels.',
-    //         ], 200);
-    //     }
-    //     catch (\Exception$e)
-    //     {
-    //         return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
-    //     }
-    // }
+            return response()->json([
+                'value'   => $vessel,
+                'message' => 'Successfully retrieved vessels.',
+            ], 200);
+        }
+        catch (\Exception$e)
+        {
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
+    }
 
     
-    // public function getVesselName(){
-    //     try {
-    //         $vessels = OpsVessel::all();
-    //         return response()->json([
-    //             'value' => collect($vessels->pluck('name'))->unique()->values()->all(),
-    //             'message' => 'Successfully retrieved vessels name.'
-    //         ], 200);
-    //     } catch (\Exception $e){
-    //         return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
-    //     }
-    // }
+    public function getVesselName(){
+        try {
+            $vessels = OpsVessel::all();
+            return response()->json([
+                'value' => collect($vessels->pluck('name'))->unique()->values()->all(),
+                'message' => 'Successfully retrieved vessels name.'
+            ], 200);
+        } catch (\Exception $e){
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
+    }
 
-    // public function getVesselWithoutPaginate(){
-    //     try
-    //     {
-    //         $vessels = OpsVessel::all();
-    //         return response()->json([
-    //             'value'   => $vessels,
-    //             'message' => 'Successfully retrieved vessels.',
-    //         ], 200);
-    //     }
-    //     catch (\Exception$e)
-    //     {
-    //         return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
-    //     }
-    // }
+    public function getVesselWithoutPaginate(){
+        try
+        {
+            $vessels = OpsVessel::all();
+            return response()->json([
+                'value'   => $vessels,
+                'message' => 'Successfully retrieved vessels.',
+            ], 200);
+        }
+        catch (\Exception$e)
+        {
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
+    }
 }
--- backend/Modules/Operations/Http/Requests/OpsVesselRequest.php
+++ backend/Modules/Operations/Http/Requests/OpsVesselRequest.php
@@ -24,24 +24,24 @@ class OpsVesselRequest extends FormRequest
      *
      * @return array
      */
-    // public function rules()
-    // {
-    //     return [
-    //         'name'            => ['required', 'string', 'max:255', Rule::unique('ops_vessels')->ignore($this->vessel)],
-    //         'code'            => ['required', 'string', 'max:255', Rule::unique('ops_vessels')->ignore($this->vessel)],
-    //         'ownership'       => ['required', 'string', 'max:50'],
-    //         'flag'            => ['required', 'string', 'max:50'],
-    //         'year_built'      => ['required', 'integer', 'min:1900', 'max:3000'],
-    //         'call_sign'       => ['required', 'alpha_num', 'max:50', Rule::unique('ops_vessels')->ignore($this->vessel)],
-    //         'grt'             => ['required', 'integer', 'min:0', 'max:10000000'],
-    //         'nrt'             => ['required', 'integer', 'min:0', 'max:10000000'],
-    //         'dwt'             => ['required'],
-    //         'speed'           => ['required'],
-    //         'capacity_tues'   => ['required', 'integer', 'min:0', 'max:10000000'],
-    //         'reefer_capacity' => ['required', 'numeric', 'min:0', 'max:10000000'],
-    //         'imo_number'      => ['required', 'alpha_num', 'max:50'],
-    //         'classification'  => ['required', 'alpha', 'max:50'],
-    //         'remarks'         => ['nullable', 'string', 'max:5000'],
-    //     ];
-    // }
+    public function rules()
+    {
+        return [
+            'name'            => ['required', 'string', 'max:255', Rule::unique('ops_vessels')->ignore($this->vessel)],
+            'code'            => ['required', 'string', 'max:255', Rule::unique('ops_vessels')->ignore($this->vessel)],
+            'ownership'       => ['required', 'string', 'max:50'],
+            'flag'            => ['required', 'string', 'max:50'],
+            'year_built'      => ['required', 'integer', 'min:1900', 'max:3000'],
+            'call_sign'       => ['required', 'alpha_num', 'max:50', Rule::unique('ops_vessels')->ignore($this->vessel)],
+            'grt'             => ['required', 'integer', 'min:0', 'max:10000000'],
+            'nrt'             => ['required', 'integer', 'min:0', 'max:10000000'],
+            'dwt'             => ['required'],
+            'speed'           => ['required'],
+            'capacity_tues'   => ['required', 'integer', 'min:0', 'max:10000000'],
+            'reefer_capacity' => ['required', 'numeric', 'min:0', 'max:10000000'],
+            'imo_number'      => ['required', 'alpha_num', 'max:50'],
+            'classification'  => ['required', 'alpha', 'max:50'],
+            'remarks'         => ['nullable', 'string', 'max:5000'],
+        ];
+    }
 }
