a220e5212a575d4a03f0ecc391826fe7c8837368
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-09-18 13:03:56 +0600

    feat: Macro added for api json response

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Response::macro('success', function ($value, $statusCode = 200) {
+            return response()->json([
+                'status' => 'success',
+                'value' => $value,
+            ], $statusCode);
+        });
+
+        Response::macro('error', function ($error, $statusCode = 400) {
+            return response()->json([
+                'message' => 'Error: ' . $error,
+            ], $statusCode);
+        });
     }
 }
