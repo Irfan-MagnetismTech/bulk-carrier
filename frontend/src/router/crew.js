db3d1f0f947ce6dec59e52c75c3229a0067ed893
ShowravBiswas <showrav.biswas1@gmail.com>
2023-10-08 17:53:42 +0600

    WIP: configure all sidebar menu

--- frontend/src/router/crew.js
+++ frontend/src/router/crew.js
@@ -3,4 +3,13 @@ const USER = Store.getters.getCurrentUser;
 
 const BASE = "crew";
 const ROLE = USER?.role ?? null;
-export default [];
+export default [
+    /* Crew Rank Management Routes */
+    // {
+    //     path: `/${BASE}/users`,
+    //     name: `${BASE}.users.index`,
+    //     component: () => import(`../views/${BASE}/user/index.vue`),
+    //     meta: { requiresAuth: true, role: ROLE, permission: 'user-show' },
+    //     props: (route) => ({ page: parseInt(route.query.page) || 1 }),
+    // },
+];
