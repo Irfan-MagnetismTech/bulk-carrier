8f88d5e75d8a05fa803313b1b6aab359aa85123c
Delowar Hossain <contact.delowar@gmail.com>
2023-10-10 12:44:00 +0600

    OPS/vessles - Route Added [In Progress]

--- frontend/src/router/operations.js
+++ frontend/src/router/operations.js
@@ -3,4 +3,13 @@ const USER = Store.getters.getCurrentUser;
 
 const BASE = "ops";
 const ROLE = USER?.role ?? null;
-export default [];
+export default [
+    /* Ports */
+	{
+		path: `/${BASE}/ports`,
+		name: `${BASE}.configurations.ports.index`,
+		component: () => import(`../views/${BASE}/user/index.vue`),
+		meta: { requiresAuth: true, role: ROLE, permission: 'user-show' },
+		props: (route) => ({ page: parseInt(route.query.page) || 1 }),
+	},
+];
--- frontend/src/services/sidebar/operations.js
+++ frontend/src/services/sidebar/operations.js
@@ -2,5 +2,68 @@ import useHeroIcon from "../../assets/heroIcon";
 const icons = useHeroIcon();
 
 export default [
-
+    {
+        route: '',
+        label: 'Operations',
+        preIcon: icons.VehicleShip,
+        postIcon: icons.DownArrow,
+        is_active: false,
+        is_open: false,
+        permissionKey: '',
+        subMenu: [
+            
+            {
+                route: '',
+                label: 'Configurations',
+                preIcon: '',
+                postIcon: icons.DownArrow,
+                is_active: false,
+                is_open: false,
+                permissionKey: '',
+                subSubMenu: [
+                    {
+                        route: 'ops.configurations.ports.index',
+                        label: 'Ports',
+                        preIcon: '',
+                        postIcon: icons.DownArrow,
+                        is_active: false,
+                        is_open: false,
+                        permissionKey: '',
+                        subSubMenu: []
+                    },
+                    // {
+                    //     route: 'ops.configurations.cargo-types.index',
+                    //     label: 'Cargo Types',
+                    //     preIcon: '',
+                    //     postIcon: icons.DownArrow,
+                    //     is_active: false,
+                    //     is_open: false,
+                    //     permissionKey: '',
+                    //     subSubMenu: []
+                    // },
+                    // {
+                    //     route: 'ops.configurations.cargo-tariffs.index',
+                    //     label: 'Cargo Tariffs',
+                    //     preIcon: '',
+                    //     postIcon: icons.DownArrow,
+                    //     is_active: false,
+                    //     is_open: false,
+                    //     permissionKey: '',
+                    //     subSubMenu: []
+                    // },
+                    // {
+                    //     route: 'ops.configurations.customers.index',
+                    //     label: 'Customers',
+                    //     preIcon: '',
+                    //     postIcon: icons.DownArrow,
+                    //     is_active: false,
+                    //     is_open: false,
+                    //     permissionKey: '',
+                    //     subSubMenu: []
+                    // },
+                ],
+            },
+            
+        ]
+    },
 ];
