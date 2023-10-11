dcd1ac91d3196cd78c7593704892f8d0a666ea09
Muhammad Mahbubur Rahman <142475315+mahbub-magnetism@users.noreply.github.com>
2023-10-11 15:51:12 +0600

    MNT/run-hour/Model modified with relationships

--- backend/Modules/Maintenance/Entities/MntRunHour.php
+++ backend/Modules/Maintenance/Entities/MntRunHour.php
@@ -4,6 +4,7 @@ namespace Modules\Maintenance\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MntRunHour extends Model
@@ -11,4 +12,14 @@ class MntRunHour extends Model
     use HasFactory;
 
     protected $fillable = ['ops_vessel_id','mnt_item_id','previous_run_hour','present_run_hour','updated_on','business_unit'];
+
+    public function opsVessel () : BelongsTo
+    {
+        return $this->belongsTo(OpsVessel::class);
+    }
+    
+    public function mntItem () : BelongsTo
+    {
+        return $this->belongsTo(MntItem::class);
+    }
 }
