a8cfa835521798042476358a5e6da7f3d8bfb4c2
Delowar Hossain <contact.delowar@gmail.com>
2023-10-11 17:22:07 +0600

    ports - Frontend Done

--- frontend/src/router/operations.js
+++ frontend/src/router/operations.js
@@ -1,15 +1,27 @@
 import Store from '../store/index.js';
 const USER = Store.getters.getCurrentUser;
 
-const BASE = "ops";
+const BASE = "operation";
 const ROLE = USER?.role ?? null;
 export default [
     /* Ports */
 	{
 		path: `/${BASE}/ports`,
 		name: `${BASE}.configurations.ports.index`,
-		component: () => import(`../views/${BASE}/user/index.vue`),
-		meta: { requiresAuth: true, role: ROLE, permission: 'user-show' },
+		component: () => import(`../views/${BASE}/port/index.vue`),
+		meta: { requiresAuth: true, role: ROLE, permission: '' },
 		props: (route) => ({ page: parseInt(route.query.page) || 1 }),
+	},
+	{
+		path: `/${BASE}/ports/create`,
+		name: `${BASE}.configurations.ports.create`,
+		component: () => import (`../views/${BASE}/port/create.vue`),
+		meta: { requiresAuth: true, role: "all", permission: '' },
+	},
+	{
+		path: `/${BASE}/ports/:portId/edit`,
+		name: `${BASE}.configurations.ports.edit`,
+		component: () => import (`../views/${BASE}/port/edit.vue`),
+		meta: { requiresAuth: true, role: "all", permission: '' },
 	},
 ];
--- frontend/src/services/sidebar/operations.js
+++ frontend/src/services/sidebar/operations.js
@@ -22,7 +22,7 @@ export default [
                 permissionKey: '',
                 subSubMenu: [
                     {
-                        route: 'ops.configurations.ports.index',
+                        route: 'operation.configurations.ports.index',
                         label: 'Ports',
                         preIcon: '',
                         postIcon: icons.DownArrow,
@@ -32,7 +32,7 @@ export default [
                         subSubMenu: []
                     },
                     // {
-                    //     route: 'ops.configurations.cargo-types.index',
+                    //     route: 'operation.configurations.cargo-types.index',
                     //     label: 'Cargo Types',
                     //     preIcon: '',
                     //     postIcon: icons.DownArrow,
@@ -42,7 +42,7 @@ export default [
                     //     subSubMenu: []
                     // },
                     // {
-                    //     route: 'ops.configurations.cargo-tariffs.index',
+                    //     route: 'operation.configurations.cargo-tariffs.index',
                     //     label: 'Cargo Tariffs',
                     //     preIcon: '',
                     //     postIcon: icons.DownArrow,
@@ -52,7 +52,7 @@ export default [
                     //     subSubMenu: []
                     // },
                     // {
-                    //     route: 'ops.configurations.customers.index',
+                    //     route: 'operation.configurations.customers.index',
                     //     label: 'Customers',
                     //     preIcon: '',
                     //     postIcon: icons.DownArrow,
