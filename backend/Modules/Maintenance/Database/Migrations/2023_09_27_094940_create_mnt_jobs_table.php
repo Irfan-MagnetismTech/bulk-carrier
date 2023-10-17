f468e6ac7e5a90a908e74392e81123b6497b603e
Muhammad Mahbubur Rahman <142475315+mahbub-magnetism@users.noreply.github.com>
2023-10-17 16:33:21 +0600

    MNT/backend-fix/remove ship department field from jobs

--- backend/Modules/Maintenance/Database/Migrations/2023_09_27_094940_create_mnt_jobs_table.php
+++ backend/Modules/Maintenance/Database/Migrations/2023_09_27_094940_create_mnt_jobs_table.php
@@ -17,8 +17,6 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('ops_vessel_id');
             $table->foreign('ops_vessel_id')->references('id')->on('ops_vessels');
-            $table->unsignedBigInteger('mnt_ship_department_id');
-            $table->foreign('mnt_ship_department_id')->references('id')->on('mnt_ship_departments');
             $table->unsignedBigInteger('mnt_item_id');
             $table->foreign('mnt_item_id')->references('id')->on('mnt_items');
             $table->enum('business_unit',['TSLL','PSML']);
