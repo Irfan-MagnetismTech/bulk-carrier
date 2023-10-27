663a0518a0ab92b341cb7e98b0228dc4e01116c4
Irfan-Majid <sirirfanmajid@gmail.com>
2023-10-27 11:12:31 +0600

    SCM/os fix

--- frontend/src/views/supply-chain/opening-stock/create.vue
+++ frontend/src/views/supply-chain/opening-stock/create.vue
@@ -24,7 +24,7 @@ setTitle('Create Opening Stock');
     </div>
     <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
         <form @submit.prevent="storeOpeningStock(openingStock)">
-          <opening-stock-form v-model:form="openingStock" v-model:materialObject="materialObject" :errors="errors"></opening-stock-form>
+          <opening-stock-form v-model:form="openingStock" :materialObject="materialObject" :errors="errors"></opening-stock-form>
             <!-- Submit button -->
             <button type="submit" :disabled="isLoading" class="flex items-center justify-between px-4 py-2 mt-4 text-sm leading-5 text-white transition-colors duration-150 bg-purple-600  border border-transparent rounded-lg fon2t-medium mt- active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">Create</button>
         </form>
