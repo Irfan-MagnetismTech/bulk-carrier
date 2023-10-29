5639bbaae6e3c9b83be6ac284c3fb8880a552d97
rabiuli <robiuli913@gmail.com>
2023-10-29 11:24:16 +0600

    OPS/Vessel-Certificates - Permanent certificate showing in certificate renew- this issue solved

--- backend/Modules/Operations/Http/Controllers/OpsVesselCertificateController.php
+++ backend/Modules/Operations/Http/Controllers/OpsVesselCertificateController.php
@@ -49,9 +49,10 @@ class OpsVesselCertificateController extends Controller
             // Calculate days difference using map
             $vesselCertificates->map(function ($certificateGroup, $vesselId) use ($currentDate) {
                 return $certificateGroup->map(function ($certificate) use ($currentDate) {                    
-                    $expireDate = Carbon::parse($certificate->expire_date);                    
-                    $expire_days = $currentDate->diffInDays($expireDate, false); 
-                    $certificate->expire_days = $expire_days;                    
+                    $expireDate = Carbon::parse($certificate->expire_date);
+                    $expire_days = $currentDate->diffInDays($expireDate, false);
+                    $certificate->expire_days = $expire_days;
+
                     return $certificate;
                 });
             });
@@ -234,12 +235,15 @@ class OpsVesselCertificateController extends Controller
             ->paginate(15)
             ->groupBy('ops_vessel_id');
 
+            // dd($vesselCertificates);
+
             $filterCertificates=$vesselCertificates->map(function ($certificateGroup, $vesselId)  use ($currentDate,$days) {
                 return $certificateGroup->filter(function ($certificate)  use ($currentDate,$days) {
                     $expireDate = Carbon::parse($certificate->expire_date);
                     $expire_days = $currentDate->diffInDays($expireDate, false);                   
-                    $certificate->expire_days = $expire_days;                  
-                    return $certificate->expire_days <= $days;
+                    $certificate->expire_days = $expire_days;
+
+                    return $certificate->expire_days <= $days && $certificate->opsMaritimeCertification->type != 'Permanent';
                 })->values();
             })->filter();
             
