221c3c7afd345071982dc9cd2f9b22eedc696b25
Irfan-Majid <sirirfanmajid@gmail.com>
2023-10-31 10:51:10 +0600

    Feat Currency composale

--- frontend/src/composables/useBusinessInfo.js
+++ frontend/src/composables/useBusinessInfo.js
@@ -1,8 +1,16 @@
 
 import Api from '../apis/Api';
+import { useLoading } from 'vue-loading-overlay';
+import useNotification from '../useNotification.js';
 
 export default function useBusinessInfo() {
 
+  const $loading = useLoading();
+  const notification = useNotification();
+  
+
+
+  
   const getAllStoreCategories = async () => {
     try {
       const { data } = await Api.get(`/scm/store-categories`);
@@ -24,6 +32,15 @@ export default function useBusinessInfo() {
       // Handle error appropriately
       console.error('Error fetching payment methods:', error);
       throw error; // Rethrow the error to be handled by the caller
+    }
+  };
+
+  const getCurrencies = async () => {
+    try {
+      const { data } = await Api.get(`/currencies`);
+      return data;
+    } catch (error) {
+      throw error;
     }
   };
 
