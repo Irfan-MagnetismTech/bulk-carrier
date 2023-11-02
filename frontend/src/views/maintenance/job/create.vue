92d2a7415a5991009e5b7b1e017e970efcb27289
joy-magnetismtech <142225000+joy-magnetismtech@users.noreply.github.com>
2023-11-02 13:33:49 +0600

    MNT/jobs - business unit readonly feature added in edit mode

--- frontend/src/views/maintenance/job/create.vue
+++ frontend/src/views/maintenance/job/create.vue
@@ -8,6 +8,7 @@ import DefaultButton from '../../../components/buttons/DefaultButton.vue';
 const icons = useHeroIcon();
 const { job, storeJob, isLoading, errors } = useJob();
 const { setTitle } = Title();
+const page = 'create';
 
 setTitle('Create Job');
 </script>
@@ -23,7 +24,7 @@ setTitle('Create Job');
     <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
         <form @submit.prevent="storeJob(job)">
             <!-- Booking Form -->
-            <job-form v-model:form="job" :errors="errors"></job-form>
+            <job-form :page="page" v-model:form="job" :errors="errors"></job-form>
             <!-- Submit button -->
             <button type="submit" :disabled="isLoading" class="flex items-center justify-between px-4 py-2 mt-4 text-sm text-white bg-purple-600 border border-transparent rounded-lg fon2t-medium mt- active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">Create Job</button>
         </form>
