53243780f7382bb34b83853cf415d2fa0df59a42
sumon-magnetism <magnetismtech@magnetismtech.com>
2023-11-02 12:37:07 +0600

    CRW/Basic-Feature - RecruitmentApproval API Created

--- backend/Modules/Crew/Http/Controllers/CrwCommonController.php
+++ backend/Modules/Crew/Http/Controllers/CrwCommonController.php
@@ -10,6 +10,7 @@ use Modules\Crew\Entities\CrwAgency;
 use Modules\Crew\Entities\CrwAgencyContract;
 use Modules\Crew\Entities\CrwCrew;
 use Modules\Crew\Entities\CrwRank;
+use Modules\Crew\Entities\CrwRecruitmentApproval;
 
 class CrwCommonController extends Controller
 {
@@ -59,6 +60,24 @@ class CrwCommonController extends Controller
             ->when(request()->crw_agency_id != null, function ($q) {
                     return $q->where('crw_agency_id',request()->crw_agency_id);
             })->get();
+
+            return response()->success('Retrieved Successfully', $crwAgencies, 200);
+        }
+        catch (QueryException $e)
+        {
+            return response()->error($e->getMessage(), 500);
+        }
+    }
+
+    public function getCrewRecruitmentApprovals()
+    {
+        try {
+
+            $crwAgencies      = CrwRecruitmentApproval::when(request()->business_unit != "ALL", function ($q)
+            {
+                $q->where('business_unit', request()->business_unit);
+            })
+            ->get();
 
             return response()->success('Retrieved Successfully', $crwAgencies, 200);
         }
