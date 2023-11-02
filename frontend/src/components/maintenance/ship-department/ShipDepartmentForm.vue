d896aad9bd43e7208a580102d9176baa1f5fbe02
joy-magnetismtech <142225000+joy-magnetismtech@users.noreply.github.com>
2023-11-02 13:16:39 +0600

    MNT/ship-departments - business unit readonly feature added in edit mode

--- frontend/src/components/maintenance/ship-department/ShipDepartmentForm.vue
+++ frontend/src/components/maintenance/ship-department/ShipDepartmentForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <business-unit-input v-model="form.business_unit"></business-unit-input>
+    <business-unit-input :page="page" v-model="form.business_unit"></business-unit-input>
     <div class="flex flex-col justify-center w-full md:flex-row md:gap-2">
         <label class="block w-full mt-2 text-sm">
             <span class="text-gray-700 dark:text-gray-300">Name <span class="text-red-500">*</span></span>
@@ -25,6 +25,10 @@ import BusinessUnitInput from "../../input/BusinessUnitInput.vue";
 
 const props = defineProps({
   form: {
+    required: false,
+    default: {}
+  },
+  page: {
     required: false,
     default: {}
   },
--- frontend/src/views/maintenance/ship-department/edit.vue
+++ frontend/src/views/maintenance/ship-department/edit.vue
@@ -9,7 +9,7 @@
       <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
           <form @submit.prevent="updateShipDepartment(shipDepartment, shipDepartmentId)">
               <!-- Booking Form -->
-            <ship-department-form v-model:form="shipDepartment" :errors="errors"></ship-department-form>
+            <ship-department-form :page="page" v-model:form="shipDepartment" :errors="errors"></ship-department-form>
               <!-- Submit button -->
               <button type="submit" class="flex items-center justify-between px-4 py-2 mt-4 text-sm leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg fon2t-medium mt- active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">Update Ship Department</button>
           </form>
@@ -31,6 +31,7 @@ import DefaultButton from '../../../components/buttons/DefaultButton.vue';
   const icons = useHeroIcon();
   
   const { setTitle } = Title();
+  const page = 'edit';
   
   setTitle('Edit Ship Department');
   
