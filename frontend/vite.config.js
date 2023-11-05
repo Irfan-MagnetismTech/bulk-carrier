ca20280b3c9a0763d11be90d8fe420c0f698d2b3
sumon-magnetism <magnetismtech@magnetismtech.com>
2023-11-05 16:19:18 +0600

    Rm dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  // mode: 'production',
+  mode: 'production',
   plugins: [vue()],
   server: { 
     host: true,
