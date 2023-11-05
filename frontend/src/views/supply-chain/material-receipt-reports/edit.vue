50da6ea0c80a002cdc4f7f210df603f9cc9ff77d
Irfan-Majid <sirirfanmajid@gmail.com>
2023-11-05 14:38:35 +0600

    mrr

---
+++ frontend/src/views/supply-chain/material-receipt-reports/edit.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {ref,onMounted} from "vue";
+
+import Title from "../../../services/title";
+import useMaterialReceiptReport from "../../../composables/supply-chain/useMaterialReceiptReport";
+import MaterialReceiptReportForm from "../../../components/supply-chain/material-receipt-reports/MaterialReceiptReportForm.vue";
+import { useRoute } from 'vue-router';
+
+const { getMaterialReceiptReports, showMaterialReceiptReport, materialReceiptReport, updateMaterialReceiptReport,materialObject, errors, isLoading } = useMaterialReceiptReport();
+
+import useHeroIcon from "../../../assets/heroIcon";
+import DefaultButton from '../../../components/buttons/DefaultButton.vue';
+
+const icons = useHeroIcon();
+
+const { setTitle } = Title();
+const route = useRoute();
+const materialReceiptReportId = route.params.materialReceiptReportId;
+const formType = 'edit';
+
+setTitle('Edit Material Receipt Report');
+
+onMounted(() => {
+    showMaterialReceiptReport(materialReceiptReportId);
+});
+</script>
+<template>
+    <!-- Heading -->
+    <div class="flex flex-col items-center justify-between w-full my-6 sm:flex-row" v-once>
+        <h2 class="text-xl font-semibold text-gray-700 dark:text-gray-200">Edit Material Receipt Report</h2>
+        <default-button :title="'Opening Stock List'" :to="{ name: 'scm.material-receipt-reports.index' }" :icon="icons.DataBase"></default-button>
+    </div>
+    <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <form @submit.prevent="updateMaterialReceiptReport(materialReceiptReport, materialReceiptReportId)">
+            <material-receipt-report-form :form="materialReceiptReport" :page="formType" :errors="errors" :formType="formType" :materialObject="materialObject"></material-receipt-report-form>
+            <!-- Submit button -->
+            <button type="submit" :disabled="isLoading" class="flex items-center justify-between px-4 py-2 mt-4 text-sm leading-5 text-white transition-colors duration-150 bg-purple-600  border border-transparent rounded-lg fon2t-medium mt- active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">Create</button>
+        </form>
+    </div>
+</template>
--- frontend/src/views/supply-chain/material-receipt-reports/index.vue
+++ frontend/src/views/supply-chain/material-receipt-reports/index.vue
@@ -2,7 +2,7 @@
 import {onMounted, watchEffect,watch,ref} from 'vue';
 import ActionButton from '../../../components/buttons/ActionButton.vue';
 import DefaultButton from '../../../components/buttons/DefaultButton.vue';
-import usePurchaseRequisition from "../../../composables/supply-chain/usePurchaseRequisition";
+import useMaterialReceiptReport from "../../../composables/supply-chain/useMaterialReceiptReport";
 import useHelper from "../../../composables/useHelper.js";
 import Title from "../../../services/title";
 import useDebouncedRef from '../../../composables/useDebouncedRef';
@@ -12,7 +12,7 @@ import useHeroIcon from "../../../assets/heroIcon";
 import { useRouter } from 'vue-router';
 
 import FilterWithBusinessUnit from "../../../components/searching/FilterWithBusinessUnit.vue";
-const { getPurchaseRequisitions, purchaseRequisitions, deletePurchaseRequisition, isLoading } = usePurchaseRequisition();
+const { getMaterialReceiptReports, materialReceiptReports, deleteMaterialReceiptReport, isLoading } = useMaterialReceiptReport();
 const { numberFormat } = useHelper();
 const { setTitle } = Title();
 const businessUnit = ref(Store.getters.getCurrentUser.business_unit);
@@ -25,28 +25,28 @@ const props = defineProps({
   },
 });
 
-const critical = ['No','Yes'];
+const settled = ['No','Yes'];
 // Code for global search start
 const columns = ["ref_no"];
 const searchKey = useDebouncedRef('', 600);
-const table = "purchase_requisitions";
+const table = "material_receipt_reports";
 
 const icons = useHeroIcon();
 
 const tableScrollWidth = ref(null);
 const screenWidth = (screen.width > 768) ? screen.width - 260 : screen.width;
 
-setTitle('Purchase Requisitions');
+setTitle('Material Receipt Reports');
 // Code for global search starts here
 
 watch(searchKey, newQuery => {
-  getPurchaseRequisitions(props.page, columns, searchKey.value, table);
+  getMaterialReceiptReports(props.page, columns, searchKey.value, table);
 });
 
 
 onMounted(() => {
   watchEffect(() => {
-    getPurchaseRequisitions(props.page,businessUnit.value)
+    getMaterialReceiptReports(props.page,businessUnit.value)
     .then(() => {
       const customDataTable = document.getElementById("customDataTable");
       if (customDataTable) {
@@ -59,33 +59,7 @@ onMounted(() => {
 });
 
 });
-// Code for global search end here
 
-const navigateToPOCreate = (purchaseRequisitionId) => {
-  const pr_id = purchaseRequisitionId; 
-  const cs_id = null;
-  const routeOptions = {
-    name: 'scm.purchase-orders.create',
-    query: {
-      pr_id: pr_id,
-      cs_id: cs_id
-    }
-  };
-  router.push(routeOptions);
-};  
-
-const navigateToMRRCreate = (purchaseRequisitionId) => {
-  const pr_id = purchaseRequisitionId; 
-  const po_id = null;
-  const routeOptions = {
-    name: 'scm.material-receipt-reports.create',
-    query: {
-      pr_id: pr_id,
-      po_id: po_id
-    }
-  };
-  router.push(routeOptions);
-};
 
 
 function confirmDelete(id) {
@@ -99,7 +73,7 @@ function confirmDelete(id) {
           confirmButtonText: 'Yes'
         }).then((result) => {
           if (result.isConfirmed) {
-            deletePurchaseRequisition(id);
+            deleteMaterialReceiptReport(id);
           }
         })
       }
@@ -109,11 +83,10 @@ function confirmDelete(id) {
   <!-- Heading -->
  
   <div class="flex items-center justify-between w-full my-3" v-once>
-    <h2 class="text-2xl font-semibold text-gray-700">Purchase Requisition List</h2>
-    <default-button :title="'Create Purchase Requisition'" :to="{ name: 'scm.purchase-requisitions.create' }" :icon="icons.AddIcon"></default-button>
+    <h2 class="text-2xl font-semibold text-gray-700">Material Receipt Report List</h2>
+    <default-button :title="'Create Material Receipt Report'" :to="{ name: 'scm.material-receipt-reports.create' }" :icon="icons.AddIcon"></default-button>
   </div>
   <div class="flex items-center justify-between mb-2 select-none">
-    <!-- <span class="w-full text-xs font-medium text-gray-500 whitespace-no-wrap">Showing {{ purchaseRequisitions?.from }}-{{ purchaseRequisitions?.to }} of {{ purchaseRequisitions?.total }}</span> -->
     <filter-with-business-unit v-model="businessUnit"></filter-with-business-unit>
     <!-- Search -->
     <div class="relative w-full">
@@ -135,42 +108,38 @@ function confirmDelete(id) {
             <th>Is Critical</th>
             <th>Purchase Center</th>
             <th>Warehouse</th>
+            <th>Create</th>
             <th>Business Unit</th>
             <th>Action</th>
           </tr>
           </thead>
           <tbody>
-            <tr v-for="(purchaseRequisition,index) in (purchaseRequisitions?.data ? purchaseRequisitions?.data : purchaseRequisitions)" :key="index">
-              <td>{{ purchaseRequisitions?.from + index }}</td>
-              <td>{{ purchaseRequisition?.ref_no }}</td>
-              <td>{{ purchaseRequisition?.raised_date }}</td>
-              <td>{{ critical[purchaseRequisition?.is_critical] }}</td>
-              <td>{{ purchaseRequisition?.purchase_center }}</td>
-              <td>{{ purchaseRequisition?.scmWarehouse?.name?? '' }}</td>
+            <tr v-for="(materialReceiptReport,index) in (materialReceiptReports?.data ? materialReceiptReports?.data : materialReceiptReports)" :key="index">
+              <td>{{ materialReceiptReports?.from + index }}</td>
+              <td>{{ materialReceiptReport?.ref_no }}</td>
+              <td>{{ materialReceiptReport?.raised_date }}</td>
+              <td>{{ materialReceiptReport?.purchase_center }}</td>
+              <td>{{ materialReceiptReport?.scmWarehouse?.name?? '' }}</td>
               <td>
-                <span :class="purchaseRequisition?.business_unit === 'PSML' ? 'text-green-700 bg-green-100' : 'text-orange-700 bg-orange-100'" class="px-2 py-1 font-semibold leading-tight rounded-full">{{ purchaseRequisition?.business_unit }}</span>
+                <span :class="materialReceiptReport?.business_unit === 'PSML' ? 'text-green-700 bg-green-100' : 'text-orange-700 bg-orange-100'" class="px-2 py-1 font-semibold leading-tight rounded-full">{{ materialReceiptReport?.business_unit }}</span>
               </td>
               <td>
-                <div class="grid grid-flow-col-dense gap-x-2">
-                  <button @click="navigateToPOCreate(purchaseRequisition.id)" class="px-2 py-1 font-semibold leading-tight rounded-full text-white bg-purple-600 hover:bg-purple-700">Create PO</button>
-                  <button @click="navigateToMRRCreate(purchaseRequisition.id)" class="px-2 py-1 font-semibold leading-tight rounded-full text-white bg-purple-600 hover:bg-purple-700">Create MRR</button>
-                  <action-button :action="'edit'" :to="{ name: 'scm.purchase-requisitions.edit', params: { purchaseRequisitionId: purchaseRequisition.id } }"></action-button>
-                  <action-button @click="confirmDelete(purchaseRequisition.id)" :action="'delete'"></action-button>
-                </div>
+                <action-button :action="'edit'" :to="{ name: 'scm.material-receipt-reports.edit', params: { materialReceiptReportId: materialReceiptReport.id } }"></action-button>
+                <action-button @click="confirmDelete(materialReceiptReport.id)" :action="'delete'"></action-button>
               </td>
             </tr>
           </tbody>
-          <tfoot v-if="!purchaseRequisitions?.data?.length" class="bg-white dark:bg-gray-800">
+          <tfoot v-if="!materialReceiptReports?.data?.length" class="bg-white dark:bg-gray-800">
         <tr v-if="isLoading">
           <td colspan="7">Loading...</td>
         </tr>
-        <tr v-else-if="!purchaseRequisitions?.data?.length">
-          <td colspan="7">No PR found.</td>
+        <tr v-else-if="!materialReceiptReports?.data?.length">
+          <td colspan="7">No MRR found.</td>
         </tr>
         </tfoot>
       </table>
     </div>
-    <Paginate :data="purchaseRequisitions" to="scm.purchase-requisitions.index" :page="page"></Paginate>
+    <Paginate :data="materialReceiptReports" to="scm.material-receipt-reports.index" :page="page"></Paginate>
   </div>
   <!-- Heading -->
   
