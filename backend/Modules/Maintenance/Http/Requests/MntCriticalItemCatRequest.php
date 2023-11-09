ec4315cc0cf92e256761b410bd8e858f5dd3ef65
Muhammad Mahbubur Rahman <142475315+mahbub-magnetism@users.noreply.github.com>
2023-11-09 14:49:51 +0600

    MNT/critical-function/MntCriticalItemCatRequest modified

--- backend/Modules/Maintenance/Http/Requests/MntCriticalItemCatRequest.php
+++ backend/Modules/Maintenance/Http/Requests/MntCriticalItemCatRequest.php
@@ -16,7 +16,7 @@ class MntCriticalItemCatRequest extends FormRequest
     {
         return [
             'mnt_critical_function_id' => 'required',
-            'category_name'  => ['required', Rule::unique('mnt_critical_item_cats')->ignore($this->id)],
+            'category_name'  => ['required', Rule::unique('mnt_critical_item_cats')->ignore($this->route('critical-item-cat'), 'id')],
         ];
     }
 
