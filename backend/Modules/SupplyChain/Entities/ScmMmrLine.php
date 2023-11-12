af45efba5b3857ef2e44db8bee57f0ecdf9a4afa
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-11-12 17:21:50 +0600

    SCM/mmr mmr curd done

---
+++ backend/Modules/SupplyChain/Entities/ScmMmrLine.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\SupplyChain\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Modules\SupplyChain\Entities\ScmMmr;
+use Modules\SupplyChain\Entities\ScmMaterial;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ScmMmrLine extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'scm_mmr_id', 'scm_material_id', 'specification', 'unit', 'quantity', 'mmr_composite_key',
+    ];
+
+    public function scmMmr(): BelongsTo
+    {
+        return $this->belongsTo(ScmMmr::class);
+    }
+
+    public function scmMaterial(): BelongsTo
+    {
+        return $this->belongsTo(ScmMaterial::class);
+    }
+}
