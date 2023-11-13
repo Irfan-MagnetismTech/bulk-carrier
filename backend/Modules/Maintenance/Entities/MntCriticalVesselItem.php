85dd7e0d27008bf4464e4694cda121cee0bd281d
Muhammad Mahbubur Rahman <142475315+mahbub-magnetism@users.noreply.github.com>
2023-11-13 17:04:12 +0600

    MNT/critical-functional/ opsVessel issue fixed

--- backend/Modules/Maintenance/Entities/MntCriticalVesselItem.php
+++ backend/Modules/Maintenance/Entities/MntCriticalVesselItem.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Modules\Operations\Entities\OpsVessel;
 
 class MntCriticalVesselItem extends Model
 {
