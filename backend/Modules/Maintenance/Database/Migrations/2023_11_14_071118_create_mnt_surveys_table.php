963f079afb13663daa6fae625392e3af40e1771d
Muhammad Mahbubur Rahman <142475315+mahbub-magnetism@users.noreply.github.com>
2023-11-14 15:12:14 +0600

    MNT/maintenance-survey/migration

---
+++ backend/Modules/Maintenance/Database/Migrations/2023_11_14_071118_create_mnt_surveys_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mnt_surveys', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ops_vessel_id');
+            $table->foreign('ops_vessel_id')->references('id')->on('ops_vessels');
+            $table->unsignedBigInteger('mnt_survey_item_id');
+            $table->foreign('mnt_survey_item_id')->references('id')->on('mnt_survey_items');
+            $table->unsignedBigInteger('mnt_survey_type_id');
+            $table->foreign('mnt_survey_type_id')->references('id')->on('mnt_survey_types');
+            $table->string('survey_name', 255);
+            $table->string('short_code', 255)->nullable();
+            $table->date('range_date_from');
+            $table->date('range_date_to');
+            $table->date('assigned_date');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mnt_surveys');
+    }
+};
