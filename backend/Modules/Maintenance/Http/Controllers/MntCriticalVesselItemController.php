82fee4cb8c8da0117b225ca39f11f180a7c99959
Muhammad Mahbubur Rahman <142475315+mahbub-magnetism@users.noreply.github.com>
2023-11-14 16:02:46 +0600

    MNT/critical-function/getCriticalVesselItems vessel wise

--- backend/Modules/Maintenance/Http/Controllers/MntCriticalVesselItemController.php
+++ backend/Modules/Maintenance/Http/Controllers/MntCriticalVesselItemController.php
@@ -164,4 +164,23 @@ class MntCriticalVesselItemController extends Controller
             return response()->error($e->getMessage(), 500);
         }
     }
+
+
+    public function getCriticalVesselItems() {
+        try {
+
+            $criticalVesselItems = MntCriticalVesselItem::with(['opsVessel:id,name','mntCriticalItem.mntCriticalItemCat.mntCriticalFunction','mntCriticalItemSps'])            
+                                ->when(request()->has('ops_vessel_id'), function($q){
+                                    $q->where('ops_vessel_id', request()->ops_vessel_id); 
+                                })
+                                ->get();
+
+            return response()->success('Critical vessel items are retrieved successfully', $criticalVesselItems, 200);
+            
+        }
+        catch (\Exception $e)
+        {
+            return response()->error($e->getMessage(), 500);
+        }
+    }
 }
