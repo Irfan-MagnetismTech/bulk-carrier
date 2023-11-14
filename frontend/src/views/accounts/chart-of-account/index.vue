822098d81c58f06572328ba801671c2af2e3be10
Delowar Hossain <contact.delowar@gmail.com>
2023-11-14 12:21:54 +0600

    Fix: Vessel Fine Tuning

--- frontend/src/views/accounts/chart-of-account/index.vue
+++ frontend/src/views/accounts/chart-of-account/index.vue
@@ -27,6 +27,11 @@ setTitle('Chart of Accounts List');
 const tableScrollWidth = ref(null);
 const screenWidth = (screen.width > 768) ? screen.width - 260 : screen.width;
 const businessUnit = ref(Store.getters.getCurrentUser.business_unit);
+let showFilter = true
+
+function swapFilter() {
+  showFilter = !showFilter
+}
 
 function confirmDelete(id) {
   Swal.fire({
@@ -94,17 +99,36 @@ onMounted(() => {
       
       <table id="example" class="w-full whitespace-no-wrap" >
           <thead v-once>
-          <tr class="w-full">
-            <th>#</th>
-            <th>Balance/Income Line</th>
-            <th>Balance/Income Line Type</th>
-            <th>Parent Account</th>
-            <th>Account Code</th>
-            <th>Account Name</th>
-            <th>Account Type</th>
-            <th>Business Unit</th>
-            <th>Action</th>
-          </tr>
+            <tr class="w-full">
+              <th class="w-12 flex border-0 items-center justify-center">
+                #
+                <button @click="swapFilter()" type="button">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+                  </svg>
+                </button>
+
+              </th>
+              <th>Balance/Income Line</th>
+              <th>Balance/Income Line Type</th>
+              <th>Parent Account</th>
+              <th>Account Code</th>
+              <th>Account Name</th>
+              <th>Account Type</th>
+              <th>Business Unit</th>
+              <th>Action</th>
+            </tr>
+            <tr class="w-full">
+              <th></th>
+              <th><input type="text" placeholder="" class="form-input" required autocomplete="off" /></th>
+              <th><input type="text" placeholder="" class="form-input" required autocomplete="off" /></th>
+              <th><input type="text" placeholder="" class="form-input" required autocomplete="off" /></th>
+              <th><input type="text" placeholder="" class="form-input" required autocomplete="off" /></th>
+              <th><input type="text" placeholder="" class="form-input" required autocomplete="off" /></th>
+              <th><input type="text" placeholder="" class="form-input" required autocomplete="off" /></th>
+              <th><input type="text" placeholder="" class="form-input" required autocomplete="off" /></th>
+              <th></th>
+            </tr>
           </thead>
           <tbody>
           <tr v-for="(chartAccountData,index) in chartOfAccounts" :key="index">
