010f72924314e8cfe39df9f2a5ce6e387e36398f
rabiuli <robiuli913@gmail.com>
2023-11-15 17:54:18 +0600

    Changes Vessel Particular export report

--- backend/Modules/Operations/Resources/views/vessel-particulars/vessel_particular.blade.php
+++ backend/Modules/Operations/Resources/views/vessel-particulars/vessel_particular.blade.php
@@ -20,8 +20,19 @@
     <table>
         <tr rowspan="3">
             <th colspan="11" style="text-align: center; 
-            font-size:20px; 
-            padding:10px;"><h2>PRINCIPAL PARTICULAR @if(isset($vesselParticular))- {{$vesselParticular->opsVessel->short_code}} @endif</h2></th>
+            font-size:20px;
+            padding:10px;"><h2>PRINCIPAL PARTICULAR @if(isset($vesselParticular))- {{$vesselParticular->opsVessel->name}} @endif</h2></th>
+        </tr>
+        <tr>
+            <td colspan="11" style="text-align: center;">{{ html_entity_decode($companyName, ENT_QUOTES, 'UTF-8') }}</td>
+        </tr>
+        <tr>
+            <td colspan="9" style="text-align: right;">Date:</td>
+            <td colspan="2">{{ now()->format('d-M-Y') }}</td>
+        </tr>
+        <tr>
+            <td colspan="9" style="text-align: right;">Time:</td>
+            <td colspan="2">{{ now()->format('g:i A') }}</td>
         </tr>
         @if(isset($vesselParticular))
             <tr>
