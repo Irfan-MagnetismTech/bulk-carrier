15d90a8e19a83d64f1e9551ccbecf70a0313447e
ShowravBiswas <showrav.biswas1@gmail.com>
2023-11-15 15:26:32 +0600

    working on acccounts module

--- frontend/src/views/accounts/bank-account/index.vue
+++ frontend/src/views/accounts/bank-account/index.vue
@@ -10,6 +10,7 @@ import useHeroIcon from "../../../assets/heroIcon";
 import Store from './../../../store/index.js';
 import FilterWithBusinessUnit from "../../../components/searching/FilterWithBusinessUnit.vue";
 import {useRouter} from "vue-router/dist/vue-router";
+import useDebouncedRef from "../../../composables/useDebouncedRef";
 const router = useRouter();
 
 const props = defineProps({
@@ -52,9 +53,90 @@ watch(
     }
 );
 
+let showFilter = ref(false);
+function swapFilter() {
+  showFilter.value = !showFilter.value;
+}
+let filterOptions = ref( {
+  "business_unit": businessUnit.value,
+  "items_per_page": 15,
+  "page": props.page,
+  "filter_options": [
+    {
+      "relation_name": "",
+      "field_name": "bank_name",
+      "search_param": "",
+      "action": null,
+      "order_by": null,
+      "date_from": null
+    },
+    {
+      "relation_name": "",
+      "field_name": "branch_name",
+      "search_param": "",
+      "action": null,
+      "order_by": null,
+      "date_from": null
+    },
+    {
+      "relation_name": "",
+      "field_name": "account_type",
+      "search_param": "",
+      "action": null,
+      "order_by": null,
+      "date_from": null
+    },
+    {
+      "relation_name": "",
+      "field_name": "account_name",
+      "search_param": "",
+      "action": null,
+      "order_by": null,
+      "date_from": null
+    },
+    {
+      "relation_name": "",
+      "field_name": "account_number",
+      "search_param": "",
+      "action": null,
+      "order_by": null,
+      "date_from": null
+    },
+    {
+      "relation_name": "",
+      "field_name": "routing_number",
+      "search_param": "",
+      "action": null,
+      "order_by": null,
+      "date_from": null
+    },
+    {
+      "relation_name": "",
+      "field_name": "contact_number",
+      "search_param": "",
+      "action": null,
+      "order_by": null,
+      "date_from": null
+    },
+    {
+      "relation_name": "",
+      "field_name": "opening_date",
+      "search_param": "",
+      "action": null,
+      "order_by": null,
+      "date_from": null
+    },
+  ]
+});
+
+function setSortingState(index,order){
+  filterOptions.value.filter_options[index].order_by = order;
+}
+
 onMounted(() => {
   watchEffect(() => {
-  getBankAccounts(props.page, businessUnit.value)
+  filterOptions.value.page = props.page;
+  getBankAccounts(filterOptions.value)
     .then(() => {
       const customDataTable = document.getElementById("customDataTable");
 
@@ -67,44 +149,130 @@ onMounted(() => {
     });
 });
 
+  filterOptions.value.filter_options.forEach((option, index) => {
+    filterOptions.value.filter_options[index].search_param = useDebouncedRef('', 800);
+  });
+
 });
 
 </script>
 
 <template>
   <!-- Heading -->
-  <div class="flex items-center justify-between w-full my-3" v-once>
+  <div class="flex items-center justify-between w-full my-3">
     <h2 class="text-2xl font-semibold text-gray-700">Bank Account List</h2>
     <default-button :title="'Create Bank Account'" :to="{ name: 'acc.bank-accounts.create' }" :icon="icons.AddIcon"></default-button>
   </div>
-  <div class="flex items-center justify-between mb-2 select-none">
-    <filter-with-business-unit v-model="businessUnit"></filter-with-business-unit>
-    <!-- Search -->
-    <div class="relative w-full">
-      <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-0 w-5 h-5 mr-2 text-gray-500 bottom-2" viewBox="0 0 20 20" fill="currentColor">
-        <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-      </svg>
-      <input type="text" placeholder="Search..." class="search" />
-    </div>
-  </div>
-
   <div id="customDataTable">
     <div  class="table-responsive max-w-screen" :class="{ 'overflow-x-auto': tableScrollWidth > screenWidth }">
       
-      <table class="w-full whitespace-no-wrap" >
-          <thead v-once>
+      <table class="w-full whitespace-no-wrap">
+          <thead>
           <tr class="w-full">
-            <th>#</th>
-            <th> Bank Name </th>
-            <th> Branch Name  </th>
-            <th> Account Type  </th>
-            <th> Account Name  </th>
-            <th> Account No  </th>
-            <th> Routing No  </th>
-            <th> Contact No  </th>
-            <th> Opening Date  </th>
-            <th>Business Unit</th>
-            <th>Action</th>
+            <th class="w-16 min-w-full">
+              <div class="w-full flex items-center justify-between">
+                # <button @click="swapFilter()" type="button" v-html="icons.FilterIcon"></button>
+              </div>
+            </th>
+            <th>
+              <div class="flex justify-evenly items-center">
+                <span>Bank Name</span>
+                <div class="flex flex-col cursor-pointer">
+                  <div v-html="icons.descIcon" @click="setSortingState(0,'asc')" :class="{ 'text-gray-800': filterOptions.filter_options[0].order_by === 'asc', 'text-gray-300': filterOptions.filter_options[0].order_by !== 'asc' }" class=" font-semibold"></div>
+                  <div v-html="icons.ascIcon" @click="setSortingState(0,'desc')" :class="{'text-gray-800' : filterOptions.filter_options[0].order_by === 'desc', 'text-gray-300' : filterOptions.filter_options[0].order_by !== 'desc' }" class=" font-semibold"></div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div class="flex justify-evenly items-center">
+                <span><nobr>Branch Name</nobr></span>
+                <div class="flex flex-col cursor-pointer">
+                  <div v-html="icons.descIcon" @click="setSortingState(1,'asc')" :class="{ 'text-gray-800': filterOptions.filter_options[1].order_by === 'asc', 'text-gray-300': filterOptions.filter_options[1].order_by !== 'asc' }" class=" font-semibold"></div>
+                  <div v-html="icons.ascIcon" @click="setSortingState(1,'desc')" :class="{ 'text-gray-800': filterOptions.filter_options[1].order_by === 'desc', 'text-gray-300': filterOptions.filter_options[1].order_by !== 'desc' }" class=" font-semibold"></div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div class="flex justify-evenly items-center">
+                <span><nobr>Account Type</nobr></span>
+                <div class="flex flex-col cursor-pointer">
+                  <div v-html="icons.descIcon" @click="setSortingState(2,'asc')" :class="{ 'text-gray-800': filterOptions.filter_options[2].order_by === 'asc', 'text-gray-300': filterOptions.filter_options[2].order_by !== 'asc' }" class=" font-semibold"></div>
+                  <div v-html="icons.ascIcon" @click="setSortingState(2,'desc')" :class="{ 'text-gray-800': filterOptions.filter_options[2].order_by === 'desc', 'text-gray-300': filterOptions.filter_options[2].order_by !== 'desc' }" class=" font-semibold"></div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div class="flex justify-evenly items-center">
+                <span><nobr>Account Name</nobr></span>
+                <div class="flex flex-col cursor-pointer">
+                  <div v-html="icons.descIcon" @click="setSortingState(3,'asc')" :class="{ 'text-gray-800': filterOptions.filter_options[3].order_by === 'asc', 'text-gray-300': filterOptions.filter_options[3].order_by !== 'asc' }" class=" font-semibold"></div>
+                  <div v-html="icons.ascIcon" @click="setSortingState(3,'desc')" :class="{ 'text-gray-800': filterOptions.filter_options[3].order_by === 'desc', 'text-gray-300': filterOptions.filter_options[3].order_by !== 'desc' }" class=" font-semibold"></div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div class="flex justify-evenly items-center">
+                <span><nobr>Account No</nobr></span>
+                <div class="flex flex-col cursor-pointer">
+                  <div v-html="icons.descIcon" @click="setSortingState(4,'asc')" :class="{ 'text-gray-800': filterOptions.filter_options[4].order_by === 'asc', 'text-gray-300': filterOptions.filter_options[4].order_by !== 'asc' }" class=" font-semibold"></div>
+                  <div v-html="icons.ascIcon" @click="setSortingState(4,'desc')" :class="{ 'text-gray-800': filterOptions.filter_options[4].order_by === 'desc', 'text-gray-300': filterOptions.filter_options[4].order_by !== 'desc' }" class=" font-semibold"></div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div class="flex justify-evenly items-center">
+                <span><nobr>Routing No</nobr></span>
+                <div class="flex flex-col cursor-pointer">
+                  <div v-html="icons.descIcon" @click="setSortingState(5,'asc')" :class="{ 'text-gray-800': filterOptions.filter_options[5].order_by === 'asc', 'text-gray-300': filterOptions.filter_options[5].order_by !== 'asc' }" class=" font-semibold"></div>
+                  <div v-html="icons.ascIcon" @click="setSortingState(5,'desc')" :class="{ 'text-gray-800': filterOptions.filter_options[5].order_by === 'desc', 'text-gray-300': filterOptions.filter_options[5].order_by !== 'desc' }" class=" font-semibold"></div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div class="flex justify-evenly items-center">
+                <span><nobr>Contact No</nobr></span>
+                <div class="flex flex-col cursor-pointer">
+                  <div v-html="icons.descIcon" @click="setSortingState(6,'asc')" :class="{ 'text-gray-800': filterOptions.filter_options[6].order_by === 'asc', 'text-gray-300': filterOptions.filter_options[6].order_by !== 'asc' }" class=" font-semibold"></div>
+                  <div v-html="icons.ascIcon" @click="setSortingState(6,'desc')" :class="{ 'text-gray-800': filterOptions.filter_options[6].order_by === 'desc', 'text-gray-300': filterOptions.filter_options[6].order_by !== 'desc' }" class=" font-semibold"></div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div class="flex justify-evenly items-center">
+                <span><nobr>Opening Date</nobr></span>
+                <div class="flex flex-col cursor-pointer">
+                  <div v-html="icons.descIcon" @click="setSortingState(7,'asc')" :class="{ 'text-gray-800': filterOptions.filter_options[7].order_by === 'asc', 'text-gray-300': filterOptions.filter_options[7].order_by !== 'asc' }" class=" font-semibold"></div>
+                  <div v-html="icons.ascIcon" @click="setSortingState(7,'desc')" :class="{ 'text-gray-800': filterOptions.filter_options[7].order_by === 'desc', 'text-gray-300': filterOptions.filter_options[7].order_by !== 'desc' }" class=" font-semibold"></div>
+                </div>
+              </div>
+            </th>
+            <th>
+              <div class="flex justify-evenly items-center">
+                <span><nobr>Business Unit</nobr></span>
+              </div>
+            </th>
+            <th class="w-20 min-w-full">Action</th>
+          </tr>
+          <tr class="w-full" v-if="showFilter">
+            <th>
+              <select v-model="filterOptions.items_per_page" class="filter_input">
+                <option value="15">15</option>
+                <option value="30">30</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+              </select>
+            </th>
+            <th><input v-model="filterOptions.filter_options[0].search_param" type="text" placeholder="" class="filter_input" autocomplete="off" /></th>
+            <th><input v-model="filterOptions.filter_options[1].search_param" type="text" placeholder="" class="filter_input" autocomplete="off" /></th>
+            <th><input v-model="filterOptions.filter_options[2].search_param" type="text" placeholder="" class="filter_input" autocomplete="off" /></th>
+            <th><input v-model="filterOptions.filter_options[3].search_param" type="text" placeholder="" class="filter_input" autocomplete="off" /></th>
+            <th><input v-model="filterOptions.filter_options[4].search_param" type="text" placeholder="" class="filter_input" autocomplete="off" /></th>
+            <th><input v-model="filterOptions.filter_options[5].search_param" type="text" placeholder="" class="filter_input" autocomplete="off" /></th>
+            <th><input v-model="filterOptions.filter_options[6].search_param" type="text" placeholder="" class="filter_input" autocomplete="off" /></th>
+            <th><input v-model="filterOptions.filter_options[7].search_param" type="date" placeholder="" class="filter_input" autocomplete="off" /></th>
+            <th>
+              <filter-with-business-unit v-model="filterOptions.business_unit"></filter-with-business-unit>
+            </th>
           </tr>
           </thead>
           <tbody>
@@ -129,10 +297,10 @@ onMounted(() => {
           </tbody>
           <tfoot v-if="!bankAccounts?.data?.length">
           <tr v-if="isLoading">
-            <td colspan="8">Loading...</td>
+            <td colspan="11">Loading...</td>
           </tr>
           <tr v-else-if="!bankAccounts?.data?.data?.length">
-            <td colspan="8">No data found.</td>
+            <td colspan="11">No data found.</td>
           </tr>
           </tfoot>
       </table>
