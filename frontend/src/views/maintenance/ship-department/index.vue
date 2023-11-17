9f0996889e7d5686cac5c91bfef1fdc5557d8db0
Muhammad Mahbubur Rahman <142475315+mahbub-magnetism@users.noreply.github.com>
2023-11-17 17:15:59 +0600

    MNT/work-requisition/align index field title

--- frontend/src/views/maintenance/ship-department/index.vue
+++ frontend/src/views/maintenance/ship-department/index.vue
@@ -181,8 +181,8 @@ onMounted(() => {
                 </div>
             </th>
             <th class="w-4/12">
-              <div class="flex justify-evenly items-center">
-                  <span>Name</span>
+              <div class="flex justify-center items-center">
+                  <span class="mr-1">Name</span>
                   <div class="flex flex-col cursor-pointer">
                     <div v-html="icons.descIcon" @click="setSortingState(0,'asc')" :class="{ 'text-gray-800': filterOptions.filter_options[0].order_by === 'asc', 'text-gray-300': filterOptions.filter_options[0].order_by !== 'asc' }" class=" font-semibold"></div>
                     <div v-html="icons.ascIcon" @click="setSortingState(0,'desc')" :class="{'text-gray-800' : filterOptions.filter_options[0].order_by === 'desc', 'text-gray-300' : filterOptions.filter_options[0].order_by !== 'desc' }" class=" font-semibold"></div>
@@ -191,8 +191,8 @@ onMounted(() => {
                 </div>
               </th>
             <th class="w-2/12">
-              <div class="flex justify-evenly items-center">
-                  <span>Short Code</span>
+              <div class="flex justify-center items-center">
+                  <span class="mr-1">Short Code</span>
                   <div class="flex flex-col cursor-pointer">
                     <div v-html="icons.descIcon" @click="setSortingState(1,'asc')" :class="{ 'text-gray-800': filterOptions.filter_options[1].order_by === 'asc', 'text-gray-300': filterOptions.filter_options[1].order_by !== 'asc' }" class=" font-semibold"></div>
                     <div v-html="icons.ascIcon" @click="setSortingState(1,'desc')" :class="{'text-gray-800' : filterOptions.filter_options[1].order_by === 'desc', 'text-gray-300' : filterOptions.filter_options[1].order_by !== 'desc' }" class=" font-semibold"></div>
