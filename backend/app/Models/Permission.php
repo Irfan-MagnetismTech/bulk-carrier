5e32269e26d206a1e227d02ee7275fe8b8b10074
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-11-17 10:47:45 +0600

    SCM/unit validation message udpate

--- backend/app/Models/Permission.php
+++ backend/app/Models/Permission.php
@@ -3,11 +3,10 @@
 namespace App\Models;
 
 use App\Traits\GlobalSearchTrait;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Permission\Models\Permission as SpatiePermission;
 
 class Permission extends SpatiePermission
 {
-    use HasFactory,GlobalSearchTrait;
+    use HasFactory, GlobalSearchTrait;
 }
