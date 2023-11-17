2e5a3894f846981a0d37fd70004c2d6b1ad4c78c
Delowar Hossain <contact.delowar@gmail.com>
2023-11-17 20:23:56 +0600

    index

--- frontend/src/config/env.js
+++ frontend/src/config/env.js
@@ -1,5 +1,5 @@
 class env {
-    static PRODUCTION = '';
+    static PRODUCTION = 'https://bulk-erp-backend.magrepo.com/';
     static DEVELOPMENT_WITH_IP = 'http://192.168.88.178/bulk-carrier-erp/backend/public/';/*'http://bulk-carrier-erp.test/';*/
     static DEVELOPMENT = 'http://bulk-carrier-erp.test/';
     static MOBILEURL = 'http://192.168.88.173:1223/';
