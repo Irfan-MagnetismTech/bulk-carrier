8a831d13386e783d0cb9de9a53b866ce68feb958
joy-magnetismtech <142225000+joy-magnetismtech@users.noreply.github.com>
2023-11-17 19:11:10 +0600

    Loader & Pagination

--- frontend/src/composables/accounts/useChartOfAccount.js
+++ frontend/src/composables/accounts/useChartOfAccount.js
@@ -27,11 +27,23 @@ export default function useChartOfAccount() {
     const filterParams = ref(null);
     const errors = ref(null);
     const isLoading = ref(false);
+    const isTableLoading = ref(false);
 
     async function getChartOfAccounts(filterOptions) {
 
-        const loader = $loading.show({'can-cancel': false, 'loader': 'dots', 'color': '#7e3af2'});
-        isLoading.value = true;
+        // const loader = $loading.show({'can-cancel': false, 'loader': 'dots', 'color': '#7e3af2'});
+        // isLoading.value = true;
+        let loader = null;
+        if (!filterOptions.isFilter) {
+            loader = $loading.show({'can-cancel': false, 'loader': 'dots', 'color': '#7e3af2'});
+            isLoading.value = true;
+            isTableLoading.value = false;
+        }
+        else {
+            isTableLoading.value = true;
+            isLoading.value = false;
+            loader?.hide();
+        }
 
         filterParams.value = filterOptions;
 
@@ -50,8 +62,16 @@ export default function useChartOfAccount() {
             const { data, status } = error.response;
             notification.showError(status);
         } finally {
-            loader.hide();
-            isLoading.value = false;
+            // loader.hide();
+            // isLoading.value = false;
+            if (!filterOptions.isFilter) {
+                loader?.hide();
+                isLoading.value = false;
+            }
+            else {
+                isTableLoading.value = false;
+                loader?.hide();
+            }
         }
     }
 
@@ -141,6 +161,7 @@ export default function useChartOfAccount() {
         updateChartOfAccount,
         deleteChartOfAccount,
         isLoading,
+        isTableLoading,
         errors,
     };
 }
