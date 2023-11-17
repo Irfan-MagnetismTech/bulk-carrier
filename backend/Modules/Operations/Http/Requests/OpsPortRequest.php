cc5629bda0f32943fff007105d141d19df357fa3
rabiuli <robiuli913@gmail.com>
2023-11-17 10:44:47 +0600

    validation message changes in port

--- backend/Modules/Operations/Http/Requests/OpsPortRequest.php
+++ backend/Modules/Operations/Http/Requests/OpsPortRequest.php
@@ -23,10 +23,24 @@ class OpsPortRequest extends FormRequest
      * @return array
      */
     public function rules()
-    {
+    {        
         return [
             'code'  => ['required','string', 'max:255', Rule::unique('ops_ports')->ignore($this->route('port'), 'id')],
             'name'  => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * 
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Port Name is required',
+            'name.max' => 'Port Name may not be greater than :max characters.',
+            'code.unique' => 'Port code is already taken',
         ];
     }
 
