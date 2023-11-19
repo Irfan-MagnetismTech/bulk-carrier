a37a6fdf1f3d8b86373cdccd3bc568aaeb728e1f
Irfan-Majid <sirirfanmajid@gmail.com>
2023-11-19 17:20:05 +0600

    mcr

---
+++ backend/Modules/SupplyChain/Http/Controllers/ScmMiController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Modules\SupplyChain\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+use Modules\SupplyChain\Entities\ScmMi;
+use Modules\SupplyChain\Services\UniqueId;
+use Modules\SupplyChain\Services\CompositeKey;
+use Modules\SupplyChain\Services\CurrentStock;
+use Modules\SupplyChain\Services\StockLedgerData;
+use Modules\SupplyChain\Http\Requests\ScmMiRequest;
+
+class ScmMiController extends Controller
+{
+
+    function __construct(private UniqueId $uniqueId, private CompositeKey $compositeKey)
+    {
+        //     $this->middleware('permission:charterer-contract-create|charterer-contract-edit|charterer-contract-show|charterer-contract-delete', ['only' => ['index','show']]);
+        //     $this->middleware('permission:charterer-contract-create', ['only' => ['store']]);
+        //     $this->middleware('permission:charterer-contract-edit', ['only' => ['update']]);
+        //     $this->middleware('permission:charterer-contract-delete', ['only' => ['destroy']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     * @return Renderable
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Renderable
+     */
+    public function create()
+    {
+        return view('supplychain::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Renderable
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function show($id)
+    {
+        return view('supplychain::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function edit($id)
+    {
+        return view('supplychain::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Renderable
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Renderable
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
