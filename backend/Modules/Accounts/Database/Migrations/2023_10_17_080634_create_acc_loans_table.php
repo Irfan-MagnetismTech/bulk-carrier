2e8d6e2e04a24bc14228f06b4421e50c33a28d89
sumon-magnetism <magnetismtech@magnetismtech.com>
2023-11-20 16:24:44 +0600

    Loan WIP

--- backend/Modules/Accounts/Database/Migrations/2023_10_17_080634_create_acc_loans_table.php
+++ backend/Modules/Accounts/Database/Migrations/2023_10_17_080634_create_acc_loans_table.php
@@ -15,8 +15,24 @@ return new class extends Migration
     {
         Schema::create('acc_loans', function (Blueprint $table) {
             $table->id();
+            $table->string('loanable_type');
+            $table->unsignedBigInteger('loanable_id');
+            $table->string('loan_type');
+            $table->string('loan_number');
+            $table->string('loan_name');
+            $table->decimal('total_sanctioned', 20, 2);
+            $table->decimal('sanctioned_limit', 20, 2);
+            $table->number('total_installment');
+            $table->float('interest_rate', );
+            $table->string('opening_date');
+            $table->string('maturity_date');
+            $table->string('emi_date');
+            $table->string('emi_amount');
+            $table->string('loan_purpose');
+            $table->string('mortgage')->nullable();
+            $table->string('remarks')->nullable();
+
 			$table->enum('business_unit', ['PSML', 'TSLL']);            
-            $table->softDeletes();
             $table->timestamps();
         });
     }
--- backend/Modules/Accounts/Entities/AccLoan.php
+++ backend/Modules/Accounts/Entities/AccLoan.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AccLoan extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = ['business_unit'];
 }
