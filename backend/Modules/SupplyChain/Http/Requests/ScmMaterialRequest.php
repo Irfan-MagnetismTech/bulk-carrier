8e988dee3748610964c11992cdb73df41127c25a
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-11-21 17:09:22 +0600

    Upload file service updated

--- backend/Modules/SupplyChain/Http/Requests/ScmMaterialRequest.php
+++ backend/Modules/SupplyChain/Http/Requests/ScmMaterialRequest.php
@@ -17,12 +17,16 @@ class ScmMaterialRequest extends FormRequest
         return [
             'name' => [
                 'required',
-                Rule::unique('scm_materials')->ignore($this->material, 'name')
+                Rule::unique('scm_materials')->ignore($this->material, 'name'),
+                'max:255',
+                'string'
             ],
 
             'material_code' => [
                 'required',
-                Rule::unique('scm_materials')->ignore($this->material, 'material_code')
+                Rule::unique('scm_materials')->ignore($this->material, 'material_code'),
+                'max:255',
+                'string'
             ],
 
         ];
@@ -38,9 +42,13 @@ class ScmMaterialRequest extends FormRequest
         return [
             'name.required' => 'Name is required',
             'name.unique' => 'Name is already taken',
+            'name.max' => 'Name is too long',
+            'name.string' => 'Name must be a string',
 
             'material_code.required' => 'Material code is required',
             'material_code.unique' => 'Material code is already taken',
+            'material_code.max' => 'Material code is too long',
+            'material_code.string' => 'Material code must be a string',
         ];
     }
 
