73714f4723031c651d1f8fd06052920d4d2a0fba
rabiuli <robiuli913@gmail.com>
2023-11-23 14:27:43 +0600

    changes in bulk report relation function

---
+++ backend/Modules/Operations/Database/Migrations/2023_11_23_082400_create_ops_bulk_noon_report_consumption_heads_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ops_bulk_noon_report_consumption_heads', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ops_bulk_noon_report_consumption_heads');
+    }
+};
