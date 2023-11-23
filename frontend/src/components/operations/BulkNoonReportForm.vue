07f89f441bae5ef897067c88d7f15d42fbc0c4ba
Delowar Hossain <contact.delowar@gmail.com>
2023-11-23 14:46:21 +0600

    WIP: Bunker Noon Report

--- frontend/src/components/operations/BulkNoonReportForm.vue
+++ frontend/src/components/operations/BulkNoonReportForm.vue
@@ -346,31 +346,52 @@
     </label>
   </div>
 
-  <div class="dt-responsive table-responsive">
-    <table id="dataTable" class="w-full table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Previous ROB</th>
-          <th>Received</th>
-          <th colspan="2">Consumption Used For</th>
-          <th>ROB</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(bunker, index) in form.opsBunkers" :key="index">
-            <td>
-              <span class="show-block">
-                {{ bunker?.name }}
-              </span>
-            </td>
-            <td>
-              <input type="text" class="form-input">
-            </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <fieldset class="px-4 pb-4 mt-3 border border-gray-700 rounded dark-disabled:border-gray-400">
+    <legend class="px-2 text-gray-700 dark-disabled:text-gray-300">Bunker Consumption</legend>
+    <div class="dt-responsive table-responsive">
+      <table id="dataTable" class="w-full table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Previous ROB</th>
+            <th>Received</th>
+            <th>Consumption Used For</th>
+            <th>ROB</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(bunker, index) in form.opsBunkers" :key="index">
+              <td>
+                <span class="show-block">
+                  {{ bunker?.name }}
+                </span>
+              </td>
+              <td>
+                <input type="text" class="form-input">
+              </td>
+              <td>
+                <input type="text" class="form-input">
+              </td>
+              <td>
+                <input type="text" class="form-input">
+              </td>
+              <td>
+                <input type="text" class="form-input">
+              </td>
+              <td>
+                <a @click="showBunkerConsumptionModal(bunker?.id)" style="display: inline-block;cursor: pointer" class="relative tooltip">
+                  <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+                  </svg>
+                  <span class="tooltiptext">Renew</span>
+                </a>
+              </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </fieldset>
 
   <div class="mt-5">
     <h4 class="text-md font-semibold my-3">Engine Info</h4>
@@ -458,7 +479,70 @@
     </div>
   </div>
 
- 
+  <div v-show="isBunkerConsumptionModalOpen" class="fixed inset-0 z-30 flex items-end overflow-y-auto bg-black bg-opacity-50 sm:items-center sm:justify-center">
+    <!-- Modal -->
+    <form @submit.prevent="" style="position: absolute;top: 0;">
+      <div class="w-full px-6 py-4 overflow-y-auto bg-white rounded-t-lg dark-disabled:bg-gray-800 sm:rounded-lg sm:m-4 sm:max-w-xl" role="dialog" id="modal">
+        <!-- Remove header if you don't want a close icon. Use modal body to place modal tile. -->
+        <header class="flex justify-end">
+          <button type="button"
+                  class="inline-flex items-center justify-center w-6 h-6 mb-2 text-gray-400 transition-colors duration-150 rounded dark-disabled:hover:text-gray-200 hover: hover:text-gray-700"
+                  aria-label="close" @click="closeCrewDocumentAddModal">
+            <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" role="img" aria-hidden="true">
+              <path
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd" fill-rule="evenodd"></path>
+            </svg>
+          </button>
+        </header>
+        <!-- Modal body -->
+        <table class="w-full mb-2 whitespace-no-wrap border-collapse contract-assign-table table2">
+          <thead v-once>
+          <tr style="background-color: #04AA6D;color: white"
+              class="text-xs font-semibold tracking-wide text-gray-500 uppercase border-b dark-disabled:border-gray-700 bg-gray-50 dark-disabled:text-gray-400 dark-disabled:bg-gray-800">
+            <th colspan="5">Add Consumption Details</th>
+          </tr>
+          </thead>
+        </table>
+
+        <div class="dt-responsive table-responsive">
+          <table id="dataTable" class="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Head</th>
+                <th>Amount</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <select v-model.trim="form.validity_period" class="form-input" required>
+                    <option value="" disabled selected>Select</option>
+                  </select>
+                </td>
+                <td>
+                  <input type="number" step="0.001" class="form-input" placeholder="Amount" />
+                </td>
+                td
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <footer class="flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark-disabled:bg-gray-800">
+          <button type="button" @click="closeCrewDocumentAddModal" style="color: #1b1e21"
+                  class="w-full px-5 py-3 text-sm font-medium leading-5 text-white text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark-disabled:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray">
+            Cancel
+          </button>
+          <button type="button"
+              class="w-full px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg sm:w-auto sm:px-4 sm:py-2 active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+            Submit
+          </button>
+        </footer>
+      </div>
+    </form>
+  </div>
   
 </template>
 <script setup>
@@ -487,20 +571,11 @@ const props = defineProps({
   engineObject: { type: Object, required: true },
 });
 
-// function fetchPorts(search, loading) {
-//       loading(true);
-//       searchPorts(search, loading)
-// }
+const isBunkerConsumptionModalOpen = ref(0);
 
-// function fetchVoyages(searchParam, loading) {
-//   loading(true)
-//   searchVoyages(searchParam, props.form.business_unit, loading)
-// }
-
-// function fetchVessels(search, loading) {
-//       loading(true);
-//       searchVessels(search, props.form.business_unit, loading);
-// }
+function showBunkerConsumptionModal(opsBunkerIndex) {
+  isBunkerConsumptionModalOpen.value = 1
+}
 function addPort() {
   props.form.opsBulkNoonReportPorts.push({...props.portObject});
 }
