0a5417806b200fc165ac4aaa20a3060a1547377d
Irfan-Majid <sirirfanmajid@gmail.com>
2023-11-26 02:11:18 +0600

    mi

--- backend/Modules/SupplyChain/Http/Requests/ScmMiShortageRequest.php
+++ backend/Modules/SupplyChain/Http/Requests/ScmMiShortageRequest.php
@@ -4,18 +4,8 @@ namespace Modules\SupplyChain\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ScmMiRequest extends FormRequest
+class ScmMiShortageRequest extends FormRequest
 {
-    protected function prepareForValidation()
-    {
-        $data =  request('data');
-        $dataArray = json_decode($data, true);
-
-        // $mergeData = array_merge($dataArray, ['attachment' => request('attachment'), 'excel' => request('excel')]);
-
-        $this->replace($dataArray);
-    }
-    
     /**
      * Get the validation rules that apply to the request.
      *
