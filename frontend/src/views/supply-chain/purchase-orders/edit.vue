9f62d64021cc9326443d7eab0cccac87e87176bf
Irfan-Majid <sirirfanmajid@gmail.com>
2023-11-27 01:51:32 +0600

    fend

--- frontend/src/views/supply-chain/purchase-orders/edit.vue
+++ frontend/src/views/supply-chain/purchase-orders/edit.vue
@@ -33,7 +33,7 @@ onMounted(() => {
     </div>
     <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark-disabled:bg-gray-800 overflow-hidden">
         <form @submit.prevent="updatePurchaseOrder(purchaseOrder, purchaseOrderId)">
-            <purchase-order-form :form="purchaseOrder" :errors="errors" :formType="formType" :materialObject="materialObject" :page="formType"></purchase-order-form>
+            <purchase-order-form v-model:form="purchaseOrder" :errors="errors" :formType="formType" :materialObject="materialObject" :page="formType"></purchase-order-form>
             <!-- Submit button -->
             <button type="submit" :disabled="isLoading" class="flex items-center justify-between px-4 py-2 mt-4 text-sm leading-5 text-white transition-colors duration-150 bg-purple-600  border border-transparent rounded-lg fon2t-medium mt- active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">Update</button>
         </form>
