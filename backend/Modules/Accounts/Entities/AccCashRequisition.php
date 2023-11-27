a48389372546ec2f547d7cb48d27dc5772aeb614
sumon-magnetism <magnetismtech@magnetismtech.com>
2023-11-27 12:54:13 +0600

    ACC/Basic-Features - Cash Requistion CRUD Completed

--- backend/Modules/Accounts/Entities/AccCashRequisition.php
+++ backend/Modules/Accounts/Entities/AccCashRequisition.php
@@ -2,13 +2,24 @@
 
 namespace Modules\Accounts\Entities;
 
+use App\Traits\GlobalSearchTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AccCashRequisition extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, GlobalSearchTrait;
 
-    protected $fillable = ['business_unit'];
+    protected $fillable = ['acc_cost_center_id', 'applied_date', 'requisitor_id', 'mpr_id', 'total_amount', 'purpose', 'business_unit']; 
+
+    public function accCashRequisitionLines()
+    {
+        return $this->hasMany(AccCashRequisitionLine::class);
+    }
+
+    public function costCenter()
+    {
+        return $this->belongsTo(AccCostCenter::class, 'acc_cost_center_id', 'id');
+    }
 }
