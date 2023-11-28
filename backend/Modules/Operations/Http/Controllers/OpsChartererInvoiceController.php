5984c04b94bf74fb98cf0e04fbad081ee1142570
rabiuli <robiuli913@gmail.com>
2023-11-28 11:36:44 +0600

    when condition value check by using isset in functions

--- backend/Modules/Operations/Http/Controllers/OpsChartererInvoiceController.php
+++ backend/Modules/Operations/Http/Controllers/OpsChartererInvoiceController.php
@@ -44,111 +44,111 @@ class OpsChartererInvoiceController extends Controller
     }
  
  
-        /**
-      * Store a newly created resource in storage.
-      * 
-      * @param OpsChartererInvoiceRequest $request
-      * @return JsonResponse
-     */
-     public function store(OpsChartererInvoiceRequest $request): JsonResponse
-     {
-         try {
-             DB::beginTransaction();
-             $charterer_invoice_info = $request->except(
-                 '_token',
-                 'opsChartererInvoiceOthers',
-                 'opsChartererInvoiceServices'
-             );
- 
-             $charterer_invoice = OpsChartererInvoice::create($charterer_invoice_info);
-             $charterer_invoice->opsChartererInvoiceOthers()->createMany($request->opsChartererInvoiceOthers);
-             $charterer_invoice->opsChartererInvoiceServices()->createMany($request->opsChartererInvoiceServices);
-             DB::commit();
-             return response()->success('Data added successfully.', $charterer_invoice, 201);
-         }
-         catch (QueryException $e)
-         {
-             DB::rollBack();
-             return response()->error($e->getMessage(), 500);
-         }
-     }
- 
-     /**
-      * Display the specified maritime certification.
-      *
-      * @param  OpsChartererInvoice  $charterer_invoice
-      * @return JsonResponse
-      */
-     public function show(OpsChartererInvoice $charterer_invoice): JsonResponse
-     {
-         $charterer_invoice->load('opsChartererProfile','opsChartererContract','opsChartererInvoiceOthers','opsChartererInvoiceServices','opsChartererInvoiceVoyages.opsVoyage.opsVoyageSectors');
-         
-         try
-         {
-             return response()->success('Data retrieved successfully.', $charterer_invoice, 200);
-         }
-         catch (QueryException $e)
-         {
-             return response()->error($e->getMessage(), 500);
-         }
- 
-     }
- 
- 
-       /**
-      * Update the specified resource in storage.
-      *
-      * @param OpsChartererInvoiceRequest $request
-      * @param  OpsChartererInvoice  $charterer_invoice
-      * @return JsonResponse
-      */
-     public function update(OpsChartererInvoiceRequest $request, OpsChartererInvoice $charterer_invoice): JsonResponse
-     {
-         try {
-             DB::beginTransaction();
-             $charterer_invoice_info = $request->except(
-                 '_token',
-                 'opsChartererInvoiceOthers',
-             );
-            
-             $charterer_invoice->update($charterer_invoice_info);       
-             $charterer_invoice->opsChartererInvoiceOthers()->delete();
-             $charterer_invoice->opsChartererInvoiceOthers()->createMany($request->opsChartererInvoiceOthers);
-             $charterer_invoice->opsChartererInvoiceServices()->delete();
-             $charterer_invoice->opsChartererInvoiceServices()->createMany($request->opsChartererInvoiceServices);
-             DB::commit();
-             return response()->success('Data updated successfully.', $charterer_invoice, 202);
-         }
-         catch (QueryException $e)
-         {            
-             DB::rollBack();
-             return response()->error($e->getMessage(), 500);
-         }
-     }
- 
-     /**
-      * Remove the specified vessel from storage.
-      *
-      * @param  OpsChartererInvoice  $charterer_invoice
-      * @return \Illuminate\Http\JsonResponse
-      */
-     public function destroy(OpsChartererInvoice $charterer_invoice): JsonResponse
-     {
-         try
-         {
+    /**
+     * Store a newly created resource in storage.
+    * 
+    * @param OpsChartererInvoiceRequest $request
+    * @return JsonResponse
+    */
+    public function store(OpsChartererInvoiceRequest $request): JsonResponse
+    {
+        try {
+            DB::beginTransaction();
+            $charterer_invoice_info = $request->except(
+                '_token',
+                'opsChartererInvoiceOthers',
+                'opsChartererInvoiceServices'
+            );
+
+            $charterer_invoice = OpsChartererInvoice::create($charterer_invoice_info);
+            $charterer_invoice->opsChartererInvoiceOthers()->createMany($request->opsChartererInvoiceOthers);
+            $charterer_invoice->opsChartererInvoiceServices()->createMany($request->opsChartererInvoiceServices);
+            DB::commit();
+            return response()->success('Data added successfully.', $charterer_invoice, 201);
+        }
+        catch (QueryException $e)
+        {
+            DB::rollBack();
+            return response()->error($e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Display the specified maritime certification.
+    *
+    * @param  OpsChartererInvoice  $charterer_invoice
+    * @return JsonResponse
+    */
+    public function show(OpsChartererInvoice $charterer_invoice): JsonResponse
+    {
+        $charterer_invoice->load('opsChartererProfile','opsChartererContract','opsChartererInvoiceOthers','opsChartererInvoiceServices','opsChartererInvoiceVoyages.opsVoyage.opsVoyageSectors');
+        
+        try
+        {
+            return response()->success('Data retrieved successfully.', $charterer_invoice, 200);
+        }
+        catch (QueryException $e)
+        {
+            return response()->error($e->getMessage(), 500);
+        }
+
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+    *
+    * @param OpsChartererInvoiceRequest $request
+    * @param  OpsChartererInvoice  $charterer_invoice
+    * @return JsonResponse
+    */
+    public function update(OpsChartererInvoiceRequest $request, OpsChartererInvoice $charterer_invoice): JsonResponse
+    {
+        try {
+            DB::beginTransaction();
+            $charterer_invoice_info = $request->except(
+                '_token',
+                'opsChartererInvoiceOthers',
+            );
+        
+            $charterer_invoice->update($charterer_invoice_info);       
+            $charterer_invoice->opsChartererInvoiceOthers()->delete();
+            $charterer_invoice->opsChartererInvoiceOthers()->createMany($request->opsChartererInvoiceOthers);
+            $charterer_invoice->opsChartererInvoiceServices()->delete();
+            $charterer_invoice->opsChartererInvoiceServices()->createMany($request->opsChartererInvoiceServices);
+            DB::commit();
+            return response()->success('Data updated successfully.', $charterer_invoice, 202);
+        }
+        catch (QueryException $e)
+        {            
+            DB::rollBack();
+            return response()->error($e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Remove the specified vessel from storage.
+    *
+    * @param  OpsChartererInvoice  $charterer_invoice
+    * @return \Illuminate\Http\JsonResponse
+    */
+    public function destroy(OpsChartererInvoice $charterer_invoice): JsonResponse
+    {
+        try
+        {
             $charterer_invoice->opsChartererInvoiceOthers()->delete();
             $charterer_invoice->opsChartererInvoiceServices()->delete();
-             $charterer_invoice->delete();
- 
-             return response()->json([
-                 'message' => 'Data deleted successfully.',
-             ], 204);
-         }
-         catch (QueryException $e)
-         {
-             return response()->error($e->getMessage(), 500);
-         }
-     }
+            $charterer_invoice->delete();
+
+            return response()->json([
+                'message' => 'Data deleted successfully.',
+            ], 204);
+        }
+        catch (QueryException $e)
+        {
+            return response()->error($e->getMessage(), 500);
+        }
+    }
      
     public function getChartererInvoiceName(){
         try {
