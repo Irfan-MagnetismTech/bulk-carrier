370988242ca92a4e4f5b5310aeaff9dea081346b
sumon-magnetism <magnetismtech@magnetismtech.com>
2023-11-28 11:32:28 +0600

    Accounts Config Modified

--- backend/config/accounts.php
+++ backend/config/accounts.php
@@ -13,17 +13,28 @@ return [
     |
      */
 
-    'balance_income_line' => [
-        'bank'                      => 8,
-        'income_from_sales'         => 49, //@ranks = Sales Accounts
-        'income_from_service'       => 57, //@ranks = Income From Service
-        'expense_for_sales'         => 75, //@ranks = Direct Expenses
-        'expense_for_service'       => 83, //@ranks = Expenses of Service
-        'fixed_assets_at_cost'      => 3, //Fixed Assets at Cost (balance_income_line_id:3)
-        'inventory'                 => 11, //Fixed Assets at Cost (balance_income_line_id:3)
+    'balance_income_base_header'    => [
+        'assets'      => 1,
+        'liabilities' => 26,
+        'revenues'    => 48,
+        'expenses'    => 74,
     ],
 
-    'account_types'       => [
+    'balance_income_balance_header' => [
+        'current_assets' => 5,
+    ],
+
+    'balance_income_line'           => [
+        'bank'              => 8,
+        'income_from_sales' => 49, //@ranks = Sales Accounts
+        'income_from_service' => 57, //@ranks = Income From Service
+        'expense_for_sales' => 75, //@ranks = Direct Expenses
+        'expense_for_service' => 83, //@ranks = Expenses of Service
+        'fixed_assets_at_cost' => 3, //Fixed Assets at Cost (balance_income_line_id:3)
+        'inventory' => 11, //Fixed Assets at Cost (balance_income_line_id:3)
+    ],
+
+    'account_types'                 => [
         'Assets'      => 1,
         'Liabilities' => 2,
         'Equity'      => 3,
