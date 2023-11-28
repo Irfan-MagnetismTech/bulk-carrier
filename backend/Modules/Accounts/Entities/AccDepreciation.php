adb19e2ed04b46f776b4609cf98cb2b182fa1b2b
sumon-magnetism <magnetismtech@magnetismtech.com>
2023-11-28 14:59:57 +0600

    ACC/Basic-Features - Asset Depreciation - CRUD Completed

--- backend/Modules/Accounts/Entities/AccDepreciation.php
+++ backend/Modules/Accounts/Entities/AccDepreciation.php
@@ -2,13 +2,28 @@
 
 namespace Modules\Accounts\Entities;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
 
 class AccDepreciation extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
-    protected $fillable = ['business_unit'];
+    /**
+     * @var array
+     */
+    protected $fillable = ['month_year', 'applied_date', 'acc_cost_center_id', 'business_unit'];
+
+    /**
+     * @return mixed
+     */
+    public function accDepreciationLines()
+    {
+        return $this->hasMany(AccDepreciationLine::class);
+    }
+
+    public function costCenter()
+    {
+        return $this->belongsTo(AccCostCenter::class, 'acc_cost_center_id', 'id');
+    }    
 }
