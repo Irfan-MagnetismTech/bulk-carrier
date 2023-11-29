ea44c655035fc39a17484de9eb0dcf69f40d2d52
Irfan-Majid <sirirfanmajid@gmail.com>
2023-11-29 15:15:42 +0600

    ft

--- frontend/src/views/supply-chain/purchase-requisitions/index.vue
+++ frontend/src/views/supply-chain/purchase-requisitions/index.vue
@@ -231,15 +231,15 @@ function confirmDelete(id) {
                 <span :class="purchaseRequisition?.business_unit === 'PSML' ? 'text-green-700 bg-green-100' : 'text-orange-700 bg-orange-100'" class="px-2 py-1 font-semibold leading-tight rounded-full">{{ purchaseRequisition?.business_unit }}</span>
               </td>
               <td>
+                <nobr>
                 <div class="grid grid-flow-col-dense gap-x-2">
-                  <nobr>
                     <button @click="navigateToPOCreate(purchaseRequisition.id)" class="px-2 py-1 font-semibold leading-tight rounded-full text-white bg-purple-600 hover:bg-purple-700">Create PO</button>
                     <button @click="navigateToMRRCreate(purchaseRequisition.id)" class="px-2 py-1 font-semibold leading-tight rounded-full text-white bg-purple-600 hover:bg-purple-700">Create MRR</button>
                     <action-button :action="'show'" :to="{ name: 'scm.purchase-requisitions.show', params: { purchaseRequisitionId: purchaseRequisition.id } }"></action-button>
                     <action-button :action="'edit'" :to="{ name: 'scm.purchase-requisitions.edit', params: { purchaseRequisitionId: purchaseRequisition.id } }"></action-button>
                     <action-button @click="confirmDelete(purchaseRequisition.id)" :action="'delete'"></action-button>
-                  </nobr>
-                </div>
+                  </div>
+                </nobr>
               </td>
             </tr>
             <LoaderComponent :isLoading = isTableLoading v-if="isTableLoading && purchaseRequisitions?.data?.length"></LoaderComponent>
