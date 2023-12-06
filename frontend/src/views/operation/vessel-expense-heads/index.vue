747be920bdc8df612596358895411d61dc24dba1
Delowar Hossain <delowar@magnetismtech.com>
2023-12-06 13:59:40 +0600

    Vessel Expense Head Show

--- frontend/src/views/operation/vessel-expense-heads/index.vue
+++ frontend/src/views/operation/vessel-expense-heads/index.vue
@@ -129,6 +129,7 @@ onMounted(() => {
                   </td>
                   <td class="items-center justify-center space-x-1 text-gray-600">
                     <nobr>
+                      <action-button :action="'show'" :to="{ name: 'ops.vessel-expense-heads.show', params: { vesselExpenseHeadId: vesselExpenseHead.id } }"></action-button>
                       <action-button :action="'edit'" :to="{ name: 'ops.vessel-expense-heads.edit', params: { vesselExpenseHeadId: vesselExpenseHead.id } }"></action-button>
                       <action-button @click="confirmDelete(vesselExpenseHead.id)" :action="'delete'"></action-button>
                     </nobr>
