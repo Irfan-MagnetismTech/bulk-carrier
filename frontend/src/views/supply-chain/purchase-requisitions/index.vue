4066c2387d93a7b47f7cb6fb4f9d4c949faf6cde
Irfan-Majid <sirirfanmajid@gmail.com>
2023-12-12 01:31:17 +0600

    fine tuning

--- frontend/src/views/supply-chain/purchase-requisitions/index.vue
+++ frontend/src/views/supply-chain/purchase-requisitions/index.vue
@@ -245,9 +245,11 @@ function confirmDelete(id) {
                       <button @click="navigateToMRRCreate(purchaseRequisition.id)" class="px-2 py-1 font-semibold leading-tight rounded-full text-white bg-purple-600 hover:bg-purple-700">Create MRR</button>
 
                     </template>
-                  
+                    <template v-if="(purchaseRequisition?.scmPos.length <= 0) && (purchaseRequisition?.scmMrrs.length <= 0) ">
+                      <action-button :action="'edit'" :to="{ name: 'scm.purchase-requisitions.edit', params: { purchaseRequisitionId: purchaseRequisition.id } }"></action-button>
+                    </template>
                     <action-button :action="'show'" :to="{ name: 'scm.purchase-requisitions.show', params: { purchaseRequisitionId: purchaseRequisition.id } }"></action-button>
-                    <action-button :action="'edit'" :to="{ name: 'scm.purchase-requisitions.edit', params: { purchaseRequisitionId: purchaseRequisition.id } }"></action-button>
+
                     <action-button @click="confirmDelete(purchaseRequisition.id)" :action="'delete'"></action-button>
                   </div>
                 </nobr>
