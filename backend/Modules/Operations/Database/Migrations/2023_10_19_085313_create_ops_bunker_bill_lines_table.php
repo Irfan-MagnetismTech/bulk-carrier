e888261c8b2ddfb2c46105ce1d4528e79dee4695
Delowar Hossain <delowar@magnetismtech.com>
2023-12-17 22:28:50 +0600

    WIP: Bunker Bill Edit

--- backend/Modules/Operations/Database/Migrations/2023_10_19_085313_create_ops_bunker_bill_lines_table.php
+++ backend/Modules/Operations/Database/Migrations/2023_10_19_085313_create_ops_bunker_bill_lines_table.php
@@ -18,12 +18,12 @@ return new class extends Migration
             $table->id();
             $table->foreignId('ops_bunker_bill_id')->constrained('ops_bunker_bills')->onDelete('cascade');
             $table->foreignIdFor(OpsBunkerRequisition::class);
-            $table->float('rate', 20, 2)->nullable();
-            $table->float('exchange_rate_bdt', 20, 2)->nullable();
-            $table->float('exchange_rate_usd', 20, 2)->nullable();
-            $table->float('amount', 20, 2)->nullable();
-            $table->float('amount_bdt', 20, 2)->nullable();
-            $table->float('amount_usd', 20, 2)->nullable();
+            $table->float('rate', 20, 4)->nullable();
+            $table->float('exchange_rate_bdt', 20, 4)->nullable();
+            $table->float('exchange_rate_usd', 20, 4)->nullable();
+            $table->float('amount', 20, 4)->nullable();
+            $table->float('amount_bdt', 20, 4)->nullable();
+            $table->float('amount_usd', 20, 4)->nullable();
             $table->text('description')->nullable();
             $table->timestamps();
         });
