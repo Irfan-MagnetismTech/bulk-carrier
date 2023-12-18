2e04a5550a3b76eae59f892559b1abc6a79017fe
sumon-magnetism <magnetismtech@magnetismtech.com>
2023-12-18 12:55:17 +0600

    ACC/Basic-Features - Administrative Salary CRUD Completed

--- backend/Modules/Accounts/Entities/AccSalary.php
+++ backend/Modules/Accounts/Entities/AccSalary.php
@@ -2,12 +2,13 @@
 
 namespace Modules\Accounts\Entities;
 
+use App\Traits\GlobalSearchTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class AccSalary extends Model
 {
-    use HasFactory;
+    use HasFactory, GlobalSearchTrait;
 
     protected $fillable = ['acc_cost_center_id','year_month','total_salary','remarks','business_unit'];
 
