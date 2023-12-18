6504244d8ede52ea8c424558a8d3bcd5645d71c5
Delowar Hossain <delowar@magnetismtech.com>
2023-12-18 16:14:48 +0600

    Vessel Expense Head : Vessel is Unique

--- backend/Modules/Operations/Http/Requests/OpsVesselExpenseHeadRequest.php
+++ backend/Modules/Operations/Http/Requests/OpsVesselExpenseHeadRequest.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Operations\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class OpsVesselExpenseHeadRequest extends FormRequest
@@ -14,7 +15,12 @@ class OpsVesselExpenseHeadRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'ops_vessel_id'           => ['required', 'integer', 'max:255'],
+            'ops_vessel_id' => [
+                'required',
+                'integer',
+                'max:255',
+                Rule::unique('ops_vessel_expense_heads', 'ops_vessel_id'),
+            ],
             // 'ops_expense_head_id'   => ['required', 'numeric', 'max:50'],
         ];
     }
@@ -27,8 +33,9 @@ class OpsVesselExpenseHeadRequest extends FormRequest
     public function messages(): array
     {
         return [
-            //
+            'ops_vessel_id.unique' => 'This Vessel already have expense heads.',
         ];
+
     }
 
     /**
