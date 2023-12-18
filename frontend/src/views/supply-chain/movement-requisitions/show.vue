2669ee7ee8c91475a5292fc19f1aa159d922c12a
Irfan-Majid <sirirfanmajid@gmail.com>
2023-12-18 15:28:57 +0600

    scm

--- frontend/src/views/supply-chain/movement-requisitions/show.vue
+++ frontend/src/views/supply-chain/movement-requisitions/show.vue
@@ -30,7 +30,7 @@ onMounted(() => {
         <h2 class="text-xl font-semibold text-gray-700 dark-disabled:text-gray-200">Movement Requisition Details</h2>
         <default-button :title="'Movement Requisition List'" :to="{ name: 'scm.movement-requisitions.index' }" :icon="icons.DataBase"></default-button>
     </div>
-    <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark-disabled:bg-gray-800 overflow-hidden">
+    <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark-disabled:bg-gray-800">
             <movement-requisition-show :form="movementRequisition"></movement-requisition-show>
     </div>
 </template>
