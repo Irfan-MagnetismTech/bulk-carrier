0615f2b0f97b21e1f436da04bcd66ed6318622c1
rabiuli <robiuli913@gmail.com>
2023-12-20 15:46:37 +0600

    amount value range changes in migration file

--- backend/Modules/Operations/Database/Migrations/2023_10_19_084652_create_ops_bunker_bills_table.php
+++ backend/Modules/Operations/Database/Migrations/2023_10_19_084652_create_ops_bunker_bills_table.php
@@ -21,9 +21,9 @@ return new class extends Migration
             $table->text('remarks')->nullable();
             $table->string('attachment')->nullable();
             $table->string('smr_file_path')->nullable();
-            $table->float('sub_total_bdt', 20, 4);
-            $table->float('discount_bdt', 20, 4)->nullable();
-            $table->float('grand_total_bdt', 20, 4)->nullable();
+            $table->float('sub_total_bdt', 20, 2);
+            $table->float('discount_bdt', 20, 2)->nullable();
+            $table->float('grand_total_bdt', 20, 2)->nullable();
             $table->enum('business_unit', ['PSML', 'TSLL','ALL']);
             $table->timestamps();
         });
