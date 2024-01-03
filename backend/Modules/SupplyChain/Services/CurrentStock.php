44c1e58476e65044aec435ba9290374cc737d164
Muhammad Mahbubur Rahman <142475315+mahbub-magnetism@users.noreply.github.com>
2024-01-03 17:33:50 +0600

    CurrentStock - countStockIn and countStockOut added

--- backend/Modules/SupplyChain/Services/CurrentStock.php
+++ backend/Modules/SupplyChain/Services/CurrentStock.php
@@ -28,4 +28,53 @@ class CurrentStock
 
         return $currentStock;
     }
+
+    
+    /**
+     * Counts the stock in of a material in a warehouse.
+     *
+     * @param int $scm_material_id
+     * @param int $scm_warehouse_id
+     * @param string|null $toDate (optional)
+     * @return int
+     */
+    public static function countStockIn($scm_material_id, $scm_warehouse_id, $toDate = null): int
+    {
+        $currentStockIn = ScmStockLedger::query()
+            ->where([
+                'scm_material_id' => $scm_material_id,
+                'scm_warehouse_id' => $scm_warehouse_id
+            ])
+            ->whereNull('recievable_type')
+            ->when(!is_null($toDate), function ($query) use ($toDate) {
+                $query->where('date', '<=', $toDate);
+            })
+            ->sum('quantity');
+
+        return $currentStockIn;
+    }
+    
+    /**
+     * Counts the stock out of a material from a warehouse.
+     *
+     * @param int $scm_material_id
+     * @param int $scm_warehouse_id
+     * @param string|null $toDate (optional)
+     * @return int
+     */
+    public static function countStockOut($scm_material_id, $scm_warehouse_id, $toDate = null): int
+    {
+        $currentStockIn = ScmStockLedger::query()
+            ->where([
+                'scm_material_id' => $scm_material_id,
+                'scm_warehouse_id' => $scm_warehouse_id
+            ])
+            ->whereNotNull('recievable_type')
+            ->when(!is_null($toDate), function ($query) use ($toDate) {
+                $query->where('date', '<=', $toDate);
+            })
+            ->sum('quantity');
+
+        return $currentStockIn;
+    }
 }
