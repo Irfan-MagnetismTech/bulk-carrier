4b25dbc6806a3e97a43901a9258bcdcc1845f926
sumon-magnetismtech <103347105+sumon-magnetismtech@users.noreply.github.com>
2024-01-04 10:30:17 +0600

    Working on Crew Appraisal Records

--- backend/Modules/Crew/Routes/api.php
+++ backend/Modules/Crew/Routes/api.php
@@ -77,8 +77,10 @@ Route::middleware(['auth:api'])->prefix('crw')->as('crw.')->group(function ()
     Route::post('get-crew-documents', [CrwCommonController::class, 'getCrewDocuments']);
     Route::post('get-crew-document-renewals', [CrwCommonController::class, 'getCrewDocumentRenewals']);
     Route::post('get-vessel-assigned-crews', [CrwCommonController::class, 'getVesselAssignedCrews']);    
+    Route::post('get-vessel-monthly-attendances', [CrwCommonController::class, 'getVesselMonthlyAttendances']);
     Route::post('get-crw-monthly-attendances', [CrwCommonController::class, 'getCrewMonthlyAttendances']);
     Route::post('get-appraisal-undone-assignments', [CrwCommonController::class, 'getAppraisalUndoneAssignments']);
+    Route::post('get-appraisal-forms', [CrwCommonController::class, 'getAppraisalForms']);
 
     Route::post('update-crew-assign-status/{crw_crew_assignment}', [CrwCrewAssignmentController::class, 'updateCrewAssignStatus']);
 
