11219b5516f469f534663e968cc9fba3ea285cc1
Irfan-Majid <sirirfanmajid@gmail.com>
2024-01-10 16:10:39 +0600

    Fine Tuning

--- frontend/src/views/supply-chain/movement-outs/create.vue
+++ frontend/src/views/supply-chain/movement-outs/create.vue
@@ -20,7 +20,7 @@ setTitle('Create Movement Out');
     <!-- Heading -->
     <div class="flex items-center justify-between w-full my-3" v-once>
         <h2 class="text-xl font-semibold text-gray-700 dark-disabled:text-gray-200">Create Movement Out</h2>
-        <default-button :title="'PR List'" :to="{ name: 'scm.movement-outs.index' }" :icon="icons.DataBase"></default-button>
+        <default-button :title="'Movement Out List'" :to="{ name: 'scm.movement-outs.index' }" :icon="icons.DataBase"></default-button>
     </div>
     <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark-disabled:bg-gray-800">
         <form @submit.prevent="storeMovementOut(movementOut)">
