e37db2b470ef732d508f8ac8d69e940a7bc864cd
joy-magnetismtech <142225000+joy-magnetismtech@users.noreply.github.com>
2024-01-10 14:27:01 +0600

    DateTime Format Function

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,3 +1,5 @@
 import moment from 'moment';
 
 export const formatDate = date => date ? moment(date).format('DD/MM/YYYY') : '';
+
+export const formatDateTime = dateTime => dateTime ? moment(dateTime).format('DD/MM/YYYY, hh:mm A') : '';
