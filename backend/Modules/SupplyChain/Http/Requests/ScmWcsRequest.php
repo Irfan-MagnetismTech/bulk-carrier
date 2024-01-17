4e81b57b3f416a18c02b8c4ec137cf3f05a4af95
rabiuli <robiuli913@gmail.com>
2024-01-17 15:47:29 +0600

    WCs- work requisition is unique in Work cs and add validation

--- backend/Modules/SupplyChain/Http/Requests/ScmWcsRequest.php
+++ backend/Modules/SupplyChain/Http/Requests/ScmWcsRequest.php
@@ -2,6 +2,7 @@
 
 namespace Modules\SupplyChain\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ScmWcsRequest extends FormRequest
@@ -25,6 +26,9 @@ class ScmWcsRequest extends FormRequest
     {
         return [
             'effective_date' => 'required|date|before_or_equal:expire_date',
+            'scmWcsServices.*.scm_wr_id' => ['required','exists:scm_wrs,id','integer', Rule::unique('scm_wcs_services')->ignore($this->route('work_c'), 'id'),],
+
+
         ];
     }
 
@@ -36,9 +40,14 @@ class ScmWcsRequest extends FormRequest
     public function messages(): array
     {
         return [
-            'effective_date.required' => 'Effective Date is required',
-            'effective_date.date' => 'Effective Date must be a date',
-            'effective_date.before_or_equal' => 'Effective Date must be before or equal to Expire Date',
+            'effective_date.required' => 'Effective Date is required.',
+            'effective_date.date' => 'Effective Date must be a date.',
+            'effective_date.before_or_equal' => 'Effective Date must be before or equal to Expire Date.',
+
+            'scmWcsServices.*.scm_wr_id.unique' => 'In row no :position Work Requisition is already taken.',
+            'scmWcsServices.*.scm_wr_id.required' => 'In row no :position Work Requisition is required.',
+            'scmWcsServices.*.scm_wr_id.integer' => 'In row no :position Work Requisition must be an integer.',
+            'scmWcsServices.*.scm_wr_id.exists' => 'In row no :position Work Requisition is not found.',
         ];
     }
 
