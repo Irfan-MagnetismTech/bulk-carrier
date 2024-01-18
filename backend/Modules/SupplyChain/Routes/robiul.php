95c2420ff8a95123c066d4ce8808c46a27acbcd3
rabiuli <robiuli913@gmail.com>
2024-01-18 15:52:16 +0600

    WCs- store quotation route added and changes url

--- backend/Modules/SupplyChain/Routes/robiul.php
+++ backend/Modules/SupplyChain/Routes/robiul.php
@@ -14,7 +14,8 @@ Route::middleware(['auth:api'])->prefix('scm')->group(function () {
     Route::get('search-wr-wise-service-for-wcs', [ScmWrController::class, "getServiceByWrIdForWcs"])->name('getServiceByWrIdForWcs');
     Route::get('search-wr', [ScmWrController::class, "searchWr"])->name('searchWr');
     Route::get('search-work-requisitions', [ScmWrController::class, "searchWorkRequisitions"])->name('search-work-requisitions');
-    Route::get('work-quotations', [ScmWcsController::class, "getWorkQuotations"])->name('work-quotations.index');
+    Route::post('wcs-quotations', [ScmWcsController::class, "storeQuotation"])->name('wcs-quotations.create');
+    Route::get('wcs-quotations', [ScmWcsController::class, "getWorkQuotations"])->name('wcs-quotations.index');
     Route::get('get-wcs-data/{id}', [ScmWcsController::class, "getWcsWiseData"])->name('getWcsWiseData');
     Route::post('close-wr', [ScmWrController::class, "closeWr"])->name('closeWr');
     Route::post('close-wrline', [ScmWrController::class, "closeWrLine"])->name('closeWrLine');
