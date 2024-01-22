7853ffa390fc1e539d20535c3255a8adb89d8a0b
Showrav Biswas <103347202+Showrav-Biswas-Mtech@users.noreply.github.com>
2024-01-22 13:10:50 +0600

    update crew rest hour record

--- frontend/src/composables/crew/useRestHourRecord.js
+++ frontend/src/composables/crew/useRestHourRecord.js
@@ -6,10 +6,12 @@ import Api from "../../apis/Api.js";
 import useNotification from '../useNotification.js';
 import Swal from 'sweetalert2';
 import cloneDeep from 'lodash/cloneDeep';
+import useDebouncedRef from "../useDebouncedRef";
 
 export default function useRestHourRecord() {
     const router = useRouter();
     const restHourRecords = ref([]);
+    const restHourReport = ref([]);
     const $loading = useLoading();
     const notification = useNotification();
 
@@ -74,8 +76,6 @@ export default function useRestHourRecord() {
 
     async function storeRestHourRecord(form) {
 
-
-
         const selectedHourlyRecords = form.hourlyRecords.filter(record => record.is_selected === true);
         const selectedCrews = form.selectedCrews.filter(record => record.is_checked === true);
         form.hourlyRecords = [];
@@ -110,6 +110,38 @@ export default function useRestHourRecord() {
         try {
             const { data, status } = await Api.get(`/crw/crw-rest-hour-entries/${restHourRecordId}`);
             restHourRecord.value = data.value;
+
+            restHourRecord.value.location_type = 'X';
+
+            //customize hourly records
+            restHourRecord.value.crwRestHourEntryLines.forEach((entryLine, entryIndex) => {
+                let hourlyRecords = restHourRecord.value.crwRestHourEntryLines[entryIndex].hourly_records;
+                restHourRecord.value.crwRestHourEntryLines[entryIndex].hourly_records = [];
+                for (let index = 0; index < 48; index++) {
+                    let existingRecord = hourlyRecords.find(record => record.hour === index);
+                    console.log(existingRecord);
+                    if(existingRecord){
+                        restHourRecord.value.crwRestHourEntryLines[entryIndex].hourly_records.push(
+                            {
+                                'is_selected': existingRecord?.is_selected,
+                                'hour'       : existingRecord?.hour,
+                                'type'       : existingRecord?.type,
+                            }
+                        );
+                    } else {
+                        restHourRecord.value.crwRestHourEntryLines[entryIndex].hourly_records.push(
+                            {
+                                'is_selected': false,
+                                'hour'       : '',
+                                'type'       : '',
+                            }
+                        );
+                    }
+                }
+            });
+            //customize hourly records
+
+
             notification.showSuccess(status);
         } catch (error) {
             const { data, status } = error.response;
@@ -122,12 +154,19 @@ export default function useRestHourRecord() {
 
     async function updateRestHourRecord(form, restHourRecordId) {
 
+        form.crwRestHourEntryLines.forEach((entryLine, entryIndex) => {
+
+            const selectedHourlyRecords = form.crwRestHourEntryLines[entryIndex].hourly_records.filter(record => record.is_selected === true);
+            form.crwRestHourEntryLines[entryIndex].hourly_records = [];
+            form.crwRestHourEntryLines[entryIndex].hourly_records = selectedHourlyRecords;
+        });
+
         const loader = $loading.show({'can-cancel': false, 'loader': 'dots', 'color': '#7e3af2'});
         isLoading.value = true;
 
         try {
             const { data, status } = await Api.put(
-                `/crw/rest-hour-records/${restHourRecordId}`,
+                `/crw/crw-rest-hour-entries/${restHourRecordId}`,
                 form
             );
             restHourRecord.value = data.value;
@@ -148,7 +187,7 @@ export default function useRestHourRecord() {
         isLoading.value = true;
 
         try {
-            const { data, status } = await Api.delete( `/crw/rest-hour-records/${restHourRecordId}`);
+            const { data, status } = await Api.delete( `/crw/crw-rest-hour-entries/${restHourRecordId}`);
             notification.showSuccess(status);
             await getRestHourRecords(filterParams.value);
         } catch (error) {
@@ -158,7 +197,26 @@ export default function useRestHourRecord() {
             loader.hide();
             isLoading.value = false;
         }
-    } 
+    }
+
+    async function getRestHourReport(form) {
+
+        const loader = $loading.show({'can-cancel': false, 'loader': 'dots', 'color': '#7e3af2'});
+        isLoading.value = true;
+
+        try {
+            const { data, status } = await Api.post('/crw/crw-rest-hour-report', form);
+            restHourReport.value = data.value;
+            //notification.showSuccess(status);
+            //await router.push({ name: "crw.rest-hour-records.index" });
+        } catch (error) {
+            const { data, status } = error.response;
+            errors.value = notification.showError(status, data);
+        } finally {
+            loader.hide();
+            isLoading.value = false;
+        }
+    }
 
     return {
         restHourRecords,
@@ -168,6 +226,8 @@ export default function useRestHourRecord() {
         showRestHourRecord,
         updateRestHourRecord,
         deleteRestHourRecord,
+        getRestHourReport,
+        restHourReport,
         isLoading,
         isTableLoading,
         errors,
