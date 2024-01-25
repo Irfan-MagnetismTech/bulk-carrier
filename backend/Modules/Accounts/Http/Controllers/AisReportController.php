a668ee502431814a2ecf871d2a0344411c0413f3
sumon-magnetismtech <103347105+sumon-magnetismtech@users.noreply.github.com>
2024-01-25 11:25:28 +0600

    Fine Tune on Cost Center Summary

--- backend/Modules/Accounts/Http/Controllers/AisReportController.php
+++ backend/Modules/Accounts/Http/Controllers/AisReportController.php
@@ -9,6 +9,7 @@ use Modules\Accounts\Entities\AccAccount;
 use Modules\Accounts\Entities\AccBalanceAndIncomeLine;
 use Modules\Accounts\Entities\AccCostCenter;
 use Modules\Accounts\Entities\AccFixedAsset;
+use Modules\Accounts\Services\CostCenterSummaryService;
 use Modules\Accounts\Services\DayBookService;
 use Modules\Accounts\Services\LedgerService;
 use Modules\Accounts\Services\TrialBalanceService;
@@ -207,51 +208,18 @@ class AisReportController extends Controller
      */
     public function costCenterSummary(Request $request)
     {
-        $balanceIncomeLine = AccBalanceAndIncomeLine::whereId($request->acc_balance_income_line_id)->first();
-        $account           = AccAccount::whereId($request->acc_account_id)->first();
+        try{
+            $costCentersInfo = (new CostCenterSummaryService)->handleCostCenterSummaryService($request); 
 
-        $costCenters = AccCostCenter::with(['ledgers' => function ($ledger)
+            return response()->json([
+                'status' => 'success',
+                'value'  => $costCentersInfo,
+            ], 200);            
+        }
+        catch (\Exception $e)
         {
-            $ledger->with('transaction:id,transaction_date')
-                ->whereHas('transaction', fn($transaction) => $transaction->where('transaction_date', '<=', request()->till_date))
-                ->when(request()->acc_account_id, fn($q) => $q->where('acc_account_id', request()->acc_account_id))
-                ->when(request()->acc_balance_income_line_id, fn($q) => $q->where('acc_balance_and_income_line_id', request()->acc_balance_income_line_id))
-                ->select(['id', 'acc_cost_center_id', 'acc_transaction_id', 'acc_account_id', 'acc_balance_and_income_line_id', 'dr_amount', 'cr_amount']);
-        }])
-        ->where('business_unit', $request->business_unit)
-        ->get();
-
-        $costCenterLines = $costCenters->map(function ($costCenter) use ($balanceIncomeLine)
-        {
-            [$prevLedgers, $currLedgers] = $costCenter->ledgers->partition(fn($q) => $q->transaction->transaction_date < request()->from_date);
-            $prevDr                      = $prevLedgers->sum('dr_amount');
-            $prevCr                      = $prevLedgers->sum('cr_amount');
-            $currDr                      = $currLedgers->sum('dr_amount');
-            $currCr                      = $currLedgers->sum('cr_amount');
-            $openingBalance              = $balanceIncomeLine->value_type == 'D' ? $prevDr - $prevCr : $prevCr - $prevDr;
-            $closingBalance              = $balanceIncomeLine->value_type == 'D' ? ($openingBalance + $currDr) - $currCr : ($openingBalance + $currCr) - $currDr;
-
-            return
-                [
-                'cost_center_name' => $costCenter->name,
-                'opening_balance' => $openingBalance,
-                'current_dr'      => $currDr,
-                'current_cr'      => $currCr,
-                'closing_balance' => $closingBalance,
-            ];
-        });
-
-        $costCentersInfo = [
-            'balance_income_line_text' => $balanceIncomeLine->line_text,
-            'account_name'             => $account?->account_name,
-            'ttl_opening'              => $costCenterLines->sum('opening_balance'),
-            'ttl_dr'                   => $costCenterLines->sum('current_dr'),
-            'ttl_cr'                   => $costCenterLines->sum('current_cr'),
-            'ttl_closing'              => $costCenterLines->sum('closing_balance'),
-            'cost_center_lines'        => $costCenterLines,
-        ];
-
-        return $costCentersInfo;
+            return response()->json(['message' => 'Error: ' . $e->getMessage()], 500);
+        }
     }
 
 }
