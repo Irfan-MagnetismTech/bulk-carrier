21784c825f1ae70625c886acaf874eecb92b7092
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2024-01-25 13:09:31 +0600

    done fine tuning

--- backend/Modules/SupplyChain/Services/UniqueId.php
+++ backend/Modules/SupplyChain/Services/UniqueId.php
@@ -14,41 +14,32 @@ class UniqueId
     /**
      * Generates a unique ID for any model.
      *
-     * @param string $model The model to generate the ID for.
-     * @param string $prefix The prefix to prepend to the ID.
-     * @return string The unique ID.
+     * @param int $modelId
+     * @param string $prefix
+     * @return string
      */
-    public static function generate(string $model, string $prefix): string
+    public static function generate(int $modelId, string $prefix)
     {
-        // $currentYear = now()->format('Y');
-        // $latestModel = $model::latest()->first();
-        // // $lastYear = $latestModel ? $latestModel->created_at->format('Y') : null;
+        // return DB::transaction(function () use ($model, $prefix) {
+        //     // Lock the row for update to prevent concurrent access
+        //     $latestModel = $model::latest()->lockForUpdate()->first();
+        //     $tableName = (new $model)->getTable();
 
-        // $tableName = (new $model)->getTable();
-        // DB::statement('SET information_schema_stats_expiry = 0');
+        //     $version = DB::select( DB::raw("select version()") )[0]->{'version()'};
+        //     return response()->json($version, 422);
 
-        // $nextId = DB::table('information_schema.tables')
-        //     ->where('table_name', $tableName)
-        //     ->where('table_schema', DB::raw('DATABASE()'))
-        //     ->value('AUTO_INCREMENT');
+        //     if (strpos($version, 'MariaDB') === false) {
+        //         DB::statement('SET information_schema_stats_expiry = 0');
+        //     }
 
-        // // $newId = ($currentYear != $lastYear) ? 1 : $nextId;
+        //     $nextId = DB::table('information_schema.tables')
+        //         ->where('table_name', $tableName)
+        //         ->where('table_schema', DB::raw('DATABASE()'))
+        //         ->value('AUTO_INCREMENT');
 
-        // return strtoupper($prefix) . '-' . ($latestModel ? $nextId : 1);
+        //     return strtoupper($prefix) . '-' . ($latestModel ? $nextId : 1);
+        // });
 
-        return DB::transaction(function () use ($model, $prefix) {
-            // Lock the row for update to prevent concurrent access
-            $latestModel = $model::latest()->lockForUpdate()->first();
-
-            $tableName = (new $model)->getTable();
-            DB::statement('SET information_schema_stats_expiry = 0');
-
-            $nextId = DB::table('information_schema.tables')
-                ->where('table_name', $tableName)
-                ->where('table_schema', DB::raw('DATABASE()'))
-                ->value('AUTO_INCREMENT');
-
-            return strtoupper($prefix) . '-' . ($latestModel ? $nextId : 1);
-        });
+            return strtoupper($prefix) . '-' . $modelId;
     }
 }
