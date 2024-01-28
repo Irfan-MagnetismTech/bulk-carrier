336e8b2d934462da11a6dd12b05754141ceb998f
Delowar Hossain <103347306+delowar-magnetism@users.noreply.github.com>
2024-01-28 11:14:45 +0600

    Added Lost DAta

--- backend/Modules/Operations/Entities/OpsVoyage.php
+++ backend/Modules/Operations/Entities/OpsVoyage.php
@@ -6,6 +6,7 @@ use App\Traits\DeletableModel;
 use App\Traits\GlobalSearchTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Operations\Entities\OpsVoyageBudget;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OpsVoyage extends Model
@@ -92,6 +93,10 @@ class OpsVoyage extends Model
 
     public function opsVoyageExpenditureEntries() {
         return $this->hasMany(OpsVoyageExpenditureEntry::class, 'ops_voyage_id','id');
+    }
+
+    public function opsVoyageBudget() {
+        return $this->hasOne(OpsVoyageBudget::class);
     }
 
 }
