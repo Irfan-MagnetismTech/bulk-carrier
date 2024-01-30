180aec8b978ee734a7ff7d665e0ef22ca41cf37e
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2024-01-30 10:37:06 +0600

    unique key  generator added

---
+++ backend/app/Services/UniqueId.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Modules\SupplyChain\Services;
+
+use App\Models\LastInserted;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * @package Modules\SupplyChain\Services
+ * 
+ * @class-type Service
+ */
+class UniqueId
+{
+    /**
+     * Generates a unique ID for any model.
+     * 
+     * @param object $model
+     * @param string $prefix
+     * 
+     * @return string
+     */
+    public static function generate(object $model, string $prefix): string
+    {
+        try {
+            DB::beginTransaction();
+
+            $lastInsertedRecord = LastInserted::query()
+                ->where('last_inserted_type', get_class($model))
+                ->lockForUpdate()
+                ->first();
+
+            if ($lastInsertedRecord) {
+                $uniqueKey = $prefix . '-' . ($lastInsertedRecord->last_inserted_id + 1);
+
+                $lastInsertedRecord->update([
+                    'last_inserted_id' => $lastInsertedRecord->last_inserted_id + 1,
+                ]);
+            } else {
+                LastInserted::query()->create([
+                    'last_inserted_type' => get_class($model),
+                    'last_inserted_id' => 1
+                ]);
+
+                $uniqueKey = $prefix . '-1';
+            }
+
+            DB::commit();
+
+            return $uniqueKey;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+}
