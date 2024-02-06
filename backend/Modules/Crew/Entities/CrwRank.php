54b4e81bb1d337fb2dd4382f663400296ca51250
sumon-magnetismtech <103347105+sumon-magnetismtech@users.noreply.github.com>
2024-02-06 13:00:44 +0600

    CRW/Basic-Features Fine Tune

--- backend/Modules/Crew/Entities/CrwRank.php
+++ backend/Modules/Crew/Entities/CrwRank.php
@@ -2,14 +2,51 @@
 
 namespace Modules\Crew\Entities;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\DeletableModel;
 use App\Traits\GlobalSearchTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Modules\Crew\Entities\CrwVesselRequiredCrewLine;
 
 class CrwRank extends Model
 {
-    use HasFactory, GlobalSearchTrait;
+    use HasFactory, GlobalSearchTrait, DeletableModel;
 
-	protected $fillable = ['name', 'short_name', 'business_unit'];
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'short_name', 'business_unit'];
+
+    /**
+     * @return mixed
+     */
+    public function crwVesselRequiredCrewLines(): HasMany
+    {
+        return $this->hasMany(CrwVesselRequiredCrewLine::class, 'crw_rank_id', 'id');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function crwCrewRequisitionLines(): HasMany
+    {
+        return $this->hasMany(CrwCrewRequisitionLine::class, 'crw_rank_id', 'id');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function crwRecruitmentApprovalLines(): HasMany
+    {
+        return $this->hasMany(CrwRecruitmentApprovalLine::class, 'crw_rank_id', 'id');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function crwCrewProfiles(): HasMany
+    {
+        return $this->hasMany(CrwCrewProfile::class, 'crw_rank_id', 'id');
+    }
 }
