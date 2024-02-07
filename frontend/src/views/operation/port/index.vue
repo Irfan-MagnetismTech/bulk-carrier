f395cb72654c224738839fbf99b929cef79d83da
rabiuli <robiuli913@gmail.com>
2024-02-07 11:46:06 +0600

    Index text align fixed

--- frontend/src/views/operation/port/index.vue
+++ frontend/src/views/operation/port/index.vue
@@ -236,14 +236,14 @@ onMounted(() => {
           </thead>
           <tbody v-if="ports?.data?.length" class="relative">
               <tr v-for="(port, index) in ports.data" :key="port?.id">
-                  <td>{{ ((paginatedPage-1) * filterOptions.items_per_page) + index + 1 }}</td>
+                  <td class="text-center">{{ ((paginatedPage-1) * filterOptions.items_per_page) + index + 1 }}</td>
 
                   <td>{{ port?.code }}</td>
                   <td>{{ port?.name }}</td>
-                  <td>
+                  <td class="text-center">
                     <span :class="port?.business_unit === 'PSML' ? 'text-green-700 bg-green-100' : 'text-orange-700 bg-orange-100'" class="px-2 py-1 font-semibold leading-tight rounded-full">{{ port?.business_unit }}</span>
                   </td>
-                  <td class="items-center justify-center space-x-1 text-gray-600">
+                  <td class="items-center text-center justify-center space-x-1 text-gray-600">
                     <nobr>
                       <action-button :action="'show'" :to="{ name: 'ops.configurations.ports.show', params: { portId: port.id } }"></action-button>
                       <action-button :action="'edit'" :to="{ name: 'ops.configurations.ports.edit', params: { portId: port.id } }"></action-button>
@@ -273,3 +273,8 @@ onMounted(() => {
     <Paginate :data="ports" to="ops.configurations.ports.index" :page="page"></Paginate>
   </div>
 </template>
+<style>
+  table > tbody> tr > td {
+      text-align: left;
+  }
+</style>
