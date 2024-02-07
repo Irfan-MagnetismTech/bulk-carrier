4652ab7c998ab65bbec0f924f7edfcdb9e2b5e2a
Irfan-Majid <sirirfanmajid@gmail.com>
2024-02-07 15:23:17 +0600

    costing wip

---
+++ backend/Modules/SupplyChain/Database/Migrations/2024_02_07_091611_create_scm_costing_lines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scm_costing_lines', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('scm_costing_id')->constrained('scm_costings')->cascadeOnDelete();
+            $table->unsignedBigInteger('scm_lc_record_id')->nullable();
+            $table->string('particulars');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scm_costing_lines');
+    }
+};
