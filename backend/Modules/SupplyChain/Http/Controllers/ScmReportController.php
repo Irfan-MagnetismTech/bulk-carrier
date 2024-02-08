8324c0845d4c2fa8c082981248c87bd9ebde1982
rabiuli <robiuli913@gmail.com>
2024-02-08 17:52:01 +0600

    SCM- stockHistoryReport api added for  report in supplychain

--- backend/Modules/SupplyChain/Http/Controllers/ScmReportController.php
+++ backend/Modules/SupplyChain/Http/Controllers/ScmReportController.php
@@ -54,5 +54,80 @@ class ScmReportController extends Controller
         }
     }
 
+    // for stock history report api
+    public function stockHistoryReport(Request $request): JsonResponse
+    {
+        try {
+            $datas = [];
+            $result = ScmStockLedger::query()
+            ->where('scm_warehouse_id', request()->scm_warehouse_id)
+            ->where('scm_material_id', request()->scm_material_id)
+            ->whereBetween('date', [request()->from_date, request()->end_date])
+            ->get()
+            ->groupBy('date')->values();
 
+
+            foreach ($result as $items) {
+                $adas = [
+                    'date' => $items[0]->date,
+                    'scmMaterial' => $items[0]->scmMaterial,
+                    'scm_warehouse_id' => $items[0]->scm_warehouse_id,
+
+                    'opening_stock' => $items->sum('quantity') ?? ($items->where('stockable_type', 'Modules\SupplyChain\Entities\ScmOpeningStock')->first()?->quantity ?? 0),
+
+                    'received' => $items->whereNull('parent_id')->sum('quantity') - (empty($result['opening_stock'])? $items->where('stockable_type', 'Modules\SupplyChain\Entities\ScmOpeningStock')->first()?->quantity ?? 0 : 0),
+
+                    'issued' => ($items->whereNotNull('parent_id')                       
+                        ->sum('quantity') * -1),
+                ];
+
+                if($adas['opening_stock'] < 0){
+                    $adas['opening_stock']=0;
+                }
+
+                $adas['closing_stock'] = max(0, $adas['opening_stock'] + $adas['received'] - $adas['issued']);
+
+                $datas[] = $adas;
+            }
+            // dd($result);
+
+            return response()->success('data', $datas, 200);
+        } catch (\Exception $e) {
+
+            return response()->error($e->getMessage(), 500);
+        }
+    }
+
+    public function purchaseRequisitionReport(Request $request): JsonResponse
+    {
+        try {
+            $datas= ScmPr::with('scmPoLines.scmPo.scmMrrs')->where('id', $request->scm_pr_id)->get();
+            
+            
+            // ->map(function($items){
+            //     $datas= $items;
+
+            //     $items->map(function($item){
+            //         $data= [];
+            //         $mrrData= [];
+            //         $data['ref_no']= $item->ref_no;
+            //         $data['status']= $item->status;
+            //         $data['closed_at']= $item->closed_at;
+            //         $data['date']= $item->date;
+
+            //         $mrrData['ref_no']= $item->ref_no;
+            //         $mrrData['status']= $item->status;
+            //         $mrrData['closed_at']= $item->closed_at;
+            //         $mrrData['date']= $item->date;
+
+            //         $mrrData
+            //     });
+            // });
+
+            return response()->success('data', $datas, 200);
+        } catch (\Exception $e) {
+
+            return response()->error($e->getMessage(), 500);
+        }
+    }
 }
