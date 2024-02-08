b65188c5450267bf21dba712f17a8e5d3c804fe5
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2024-02-08 15:43:42 +0600

    added featuers as per menu

--- backend/Modules/SupplyChain/Entities/ScmPo.php
+++ backend/Modules/SupplyChain/Entities/ScmPo.php
@@ -53,8 +53,11 @@ class ScmPo extends Model
 
     protected $skipForDeletionCheck = ['scmPoLines', 'scmPoTerms', 'scmPoItems'];
 
-    protected $features = 'CS, LC Record and MRR';
-    
+    protected $features = [
+        'scmLcRecords' => 'LC Records',
+        'scmMrrs' => 'Material Receipt Reports',
+    ];
+
     public function scmPoLines(): HasMany
     {
         return $this->hasMany(ScmPoLine::class);
