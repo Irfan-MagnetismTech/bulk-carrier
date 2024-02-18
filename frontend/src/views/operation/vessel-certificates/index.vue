14cec9e5e6bdbb5fbd3f4c8d084a1a13884db606
Delowar Hossain <103347306+delowar-magnetism@users.noreply.github.com>
2024-02-18 16:18:06 +0600

    Vessel Certificate Index Downloadable

--- frontend/src/views/operation/vessel-certificates/index.vue
+++ frontend/src/views/operation/vessel-certificates/index.vue
@@ -17,6 +17,9 @@ import LoaderComponent from "../../../components/utils/LoaderComponent.vue";
 import ErrorComponent from "../../../components/utils/ErrorComponent.vue";
 import FilterComponent from "../../../components/utils/FilterComponent.vue";
 import { formatDate } from "../../../utils/helper";
+import FileExportButton from "../../../components/buttons/FileExportButton.vue";
+
+
 const router = useRouter();
 const debouncedValue = useDebouncedRef('', 800);
 
@@ -36,6 +39,8 @@ setTitle('Vessel Certificate List');
 const tableScrollWidth = ref(null);
 const screenWidth = (screen.width > 768) ? screen.width - 260 : screen.width;
 const businessUnit = ref(Store.getters.getCurrentUser.business_unit);
+const rightAlign = [];
+const leftAlign = [1,2];
 
 function confirmDelete(id) {
   Swal.fire({
@@ -189,13 +194,23 @@ onMounted(() => {
   <!-- Heading -->
   <div class="flex items-center justify-between w-full my-3" v-once>
     <h2 class="text-2xl font-semibold text-gray-700">Vessel Certificate List</h2>
-    <default-button :title="'Create Vessel Certificate'" :to="{ name: 'ops.vessel-certificates.create' }" :icon="icons.AddIcon"></default-button>
+    <div class="flex gap-2">
+      <default-button :title="'Create Vessel Certificate'" :to="{ name: 'ops.vessel-certificates.create' }" :icon="icons.AddIcon"></default-button>
+      <file-export-button
+        :businessUnit="businessUnit"
+        :pageOrientation="'l'"
+        :fileName="'Vessel Certificate List'"
+        :tableId="'vessel-certificate-list'"
+        :leftAlign="leftAlign"
+        :rightAlign="rightAlign"
+      ></file-export-button>
+    </div>
   </div>
 
   <div id="customDataTable">
     <div  class="table-responsive max-w-screen" :class="{ 'overflow-x-auto': tableScrollWidth > screenWidth }">
       
-      <table class="w-full whitespace-no-wrap" >
+      <table class="w-full whitespace-no-wrap" id="vessel-certificate-list">
           <!-- <thead v-once>
           <tr class="w-full">
             <th>#</th>
