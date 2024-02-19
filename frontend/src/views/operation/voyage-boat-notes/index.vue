79582e226be9e456f55aa9dfeb8277ba9158dddb
Delowar Hossain <103347306+delowar-magnetism@users.noreply.github.com>
2024-02-19 11:44:40 +0600

    Voyage Boat Note List Downloadable

--- frontend/src/views/operation/voyage-boat-notes/index.vue
+++ frontend/src/views/operation/voyage-boat-notes/index.vue
@@ -15,6 +15,8 @@ import useDebouncedRef from "../../../composables/useDebouncedRef";
 import LoaderComponent from "../../../components/utils/LoaderComponent.vue";
 import ErrorComponent from "../../../components/utils/ErrorComponent.vue";
 import FilterComponent from "../../../components/utils/FilterComponent.vue";
+import FileExportButton from "../../../components/buttons/FileExportButton.vue";
+
 const router = useRouter();
 const debouncedValue = useDebouncedRef('', 800);
 
@@ -120,7 +122,8 @@ let filterOptions = ref( {
 let stringifiedFilterOptions = JSON.stringify(filterOptions.value);
 const currentPage = ref(1);
 const paginatedPage = ref(1);
-
+const rightAlign = [];
+const leftAlign = [2];
 
 onMounted(() => {
   watchPostEffect(() => {
@@ -160,7 +163,17 @@ onMounted(() => {
   <!-- Heading -->
   <div class="flex items-center justify-between w-full my-3" v-once>
     <h2 class="text-2xl font-semibold text-gray-700">Voyage Boat Note List</h2>
-    <default-button :title="'Create Voyage Boat Note'" :to="{ name: 'ops.voyage-boat-notes.create' }" :icon="icons.AddIcon"></default-button>
+    <div class="flex gap-2">
+      <default-button :title="'Create Voyage Boat Note'" :to="{ name: 'ops.voyage-boat-notes.create' }" :icon="icons.AddIcon"></default-button>
+      <file-export-button
+        :businessUnit="businessUnit"
+        :pageOrientation="'l'"
+        :fileName="'Voyage Boat Note List'"
+        :tableId="'boat-note-list'"
+        :leftAlign="leftAlign"
+        :rightAlign="rightAlign"
+      ></file-export-button>
+    </div>
   </div>
   <!-- <div class="flex items-center justify-between mb-2 select-none">
     <filter-with-business-unit v-model="businessUnit"></filter-with-business-unit>
@@ -176,7 +189,7 @@ onMounted(() => {
   <div id="customDataTable">
     <div  class="table-responsive max-w-screen" :class="{ 'overflow-x-auto': tableScrollWidth > screenWidth }">
       
-      <table class="w-full whitespace-no-wrap" >
+      <table class="w-full whitespace-no-wrap" id="boat-note-list">
           <!-- <thead v-once>
           <tr class="w-full">
             <th>#</th>
